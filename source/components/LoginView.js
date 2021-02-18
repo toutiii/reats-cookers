41073ff4d99238aec6d20cde22c3cf09c48e54d2
tout_yong <twotell75@gmail.com>
2021-02-18 18:53:39 +0100

    First version of LoginView.

---
+++ source/components/LoginView.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import { Alert, Image, Text, TextInput, TouchableOpacity } from 'react-native';
+import { Appearance } from 'react-native-appearance'
+import { LinearGradient } from 'expo-linear-gradient';
+import all_constants from '../constants'
+import styles from '../styles/styles-login-view.js'
+
+
+export default class LoginView extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            email: '',
+            pwd: '',
+            isEmailFocused: false,
+            isPwdFocused: false,
+            appearance: Appearance.getColorScheme(),
+            textInputBorderColor: ''
+        }
+        this.colors = ['red', 'yellow', 'green']
+        this.colors = this.colors.sort(() => 0.5 - Math.random())
+        this.images_keys = Object.keys(all_constants.stars_images).sort(() => 0.5 - Math.random())
+    }
+    onFocusEmailChange = () => {
+        this.setState({ isEmailFocused: true });
+        this.setState({ textInputBorderColor: this.appearance === 'dark' ? 'green': 'yellow' });
+    }
+    onFocusPwdChange = () => {
+        this.setState({ isPwdFocused: true });
+    }
+    onBlurEmailChange = () => {
+        this.setState({ isEmailFocused: false });
+        this.setState({ textInputBorderColor: this.appearance === 'dark' ? 'green': 'yellow' });
+    }
+    onBlurPwdChange = () => {
+        this.setState({isPwdFocused: false});
+    }
+    render() {
+        console.log(this.state.appearance)
+        console.log(this.state.textInputBorderColor)
+        const first_image = this.images_keys[0]
+        return (
+            <LinearGradient
+                colors={this.colors}
+                style={styles.background}
+            >
+                <Image
+                    style={styles.star}
+                    source={all_constants.stars_images[first_image]}
+                />
+                <Text style={styles.welcome_text}>
+                    Welcome on Afro Eats !
+                </Text>
+                <TextInput
+                    style={
+                        [
+                            styles.text_input, styles.text_input_email_top,
+                            { fontStyle: this.state.email ? 'normal' : 'italic'},
+                            { borderColor: this.state.isEmailFocused ? this.state.textInputBorderColor : 'white'},
+                        ]
+                    }
+                    placeholder='Your Email'
+                    placeholderTextColor='white'
+                    onChangeText={email => this.setState({email})}
+                    onFocus={ this.onFocusEmailChange }
+                    onBlur={ this.onBlurEmailChange }
+                    value={this.state.email}
+                />
+                <TextInput
+                    style={
+                        [
+                            styles.text_input, styles.text_input_pwd_top,
+                            { fontStyle: this.state.pwd ? 'normal' : 'italic'},
+                            { borderColor: this.state.isPwdFocused ? this.state.textInputBorderColor : 'white'},
+                        ]
+                    }
+                    placeholder='Your password'
+                    placeholderTextColor='white'
+                    onChangeText={pwd => this.setState({pwd})}
+                    onFocus={ this.onFocusPwdChange }
+                    onBlur={ this.onBlurPwdChange }
+                    value={this.state.pwd}
+                />
+                <TouchableOpacity
+                    style={styles.login_button}
+                    onPress={() => Alert.alert('ZA WARUUDO !')}
+                >
+                    <Text style={{ fontSize: 17 }}> CONNEXION </Text>
+                </TouchableOpacity>
+            </LinearGradient>
+        )
+    }
+}
