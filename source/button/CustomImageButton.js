4ada755fbb7b47d26b491635727f36315e3e3c3d
tout_yong <twotell75@gmail.com>
2021-03-05 18:43:31 +0100

    Reusable component to easily create a clickable image when needed.

---
+++ source/button/CustomImageButton.js
@@ -0,0 +1,27 @@
+import {Image, Text, TouchableHighlight, View} from "react-native";
+import styles from "../styles/styles";
+import React from "react";
+import styles_home_view from "../styles/styles-home-view";
+import all_constants from "../constants";
+
+
+export default function CustomImageButton ({label, ...props}) {
+    return (
+        <TouchableHighlight
+            {...props}
+            onPress={props.onPress}
+            style={
+                [
+                    styles_home_view.home_button,
+                ]
+            }
+        >
+            <View style={{alignItems: 'center'}}>
+                <Image
+                    source={{uri: props.uri}}
+                    style={{height: 30, width: 30}}
+                />
+            </View>
+        </TouchableHighlight>
+    )
+}
