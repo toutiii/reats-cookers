a92baaf88af65d9977d70b942c263ad436cdd8f4
tout_yong <twotell75@gmail.com>
2021-03-05 18:43:05 +0100

    Specific style for HomeView.

---
+++ source/styles/styles-home-view.js
@@ -0,0 +1,66 @@
+import { StyleSheet } from "react-native";
+import all_constants from "../constants";
+
+
+let styles_home_view;
+
+export default styles_home_view = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 50,
+        marginBottom: 100
+    },
+    sub_container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 3,
+        width: '95%',
+        borderRadius: 20,
+    },
+    text:{
+        fontSize: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    online_button:{
+        borderWidth: 2,
+        borderRadius: 20,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    online_button_text:{
+        alignItems: 'center',
+        fontSize: 17,
+        justifyContent:'center',
+        color: 'white'
+    },
+    profile_pic: {
+        flex: 1,
+        aspectRatio: 1,
+        borderRadius: 50,
+    },
+   home_button: {
+       flex: 1,
+       width: '100%',
+       borderRadius:30,
+       justifyContent: 'center'
+   },
+    label_view: {
+        flex:1,
+        alignSelf: 'stretch',
+        justifyContent: 'center',
+        paddingLeft: 10,
+        backgroundColor: '#d3d3d3'
+    },
+    order_view_style: {
+        flex: 1,
+        flexDirection: 'row',
+        width: '95%',
+        alignItems: 'center',
+        borderBottomWidth: 1
+    }
+})
