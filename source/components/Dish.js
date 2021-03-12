95961930463316061f9a3881e38aa98af5b2af5f
tout_yong <twotell75@gmail.com>
2021-03-12 23:00:34 +0100

    Dish.js has to only be a reusable custom component, not a button.

--- source/components/Dish.js
+++ source/components/Dish.js
@@ -1,49 +1,39 @@
-import React from "react";
-import {Image, Text, TouchableHighlight, View} from "react-native";
-import styles_dish from '../styles/styles-dish'
+import {Image, Text, View} from "react-native";
+import styles_dish from "../styles/styles-dish";
 import all_constants from "../constants";
-import {useColorScheme} from 'react-native-appearance';
+import React from "react";
 import HorizontalLine from "./HorizontalLine";
 
 
-export default function Dish({...props}) {
-    const colorScheme = useColorScheme();
-    return (
-        <View style={[
-            styles_dish.dish_container,
-            {borderColor: colorScheme === 'light' ? 'black': 'white'}
-        ]}>
-            <TouchableHighlight
-                onPress={props.onPress}
-                style={{flex: 1}}
-                underlayColor={all_constants.colors.inputBorderColor}
-            >
-                <View style={{flex: 1,}}>
-                    <View style={{flex: 2,}}>
-                        <Image
-                            source={{uri: props.dish_photo}}
-                            style={styles_dish.images}
-                        />
+export default function Dish ({...props}) {
+    return(
+        <View style={{flex: 1,}}>
+            <View style={{flex: 1,}}>
+                <View style={{flex: 2,}}>
+                    <Image
+                        source={{uri: props.dish_photo}}
+                        style={styles_dish.images}
+                    />
+                </View>
+                <View style={{flex: 1}}>
+                    <View style={styles_dish.dish_price}>
+                        <Text style={{fontSize: 20}}> {props.dish_price} </Text>
                     </View>
-                    <View style={{flex: 1}}>
-                        <View style={styles_dish.dish_price}>
-                            <Text style={{fontSize: 20}}> {props.dish_price} </Text>
-                        </View>
-                        <View style={styles_dish.dish_name}>
-                            <Text numberOfLines={1} style={{fontSize: 20}}> {props.dish_name} </Text>
-                        </View>
-                        <View style={styles_dish.dish_rating}>
-                            <Image
-                                source={{uri: all_constants.uri.rating_star}}
-                                style={styles_dish.rating_star}
-                            />
-                            <Text style={{fontSize: 14}}> {props.dish_rating} </Text>
-                        </View>
+                    <View style={styles_dish.dish_name}>
+                        <Text numberOfLines={1} style={{fontSize: 20}}> {props.dish_name} </Text>
+                    </View>
+                    <View style={styles_dish.dish_rating}>
+                        <Image
+                            source={{uri: all_constants.uri.rating_star}}
+                            style={styles_dish.rating_star}
+                        />
+                        <Text style={{fontSize: 14}}> {props.dish_rating} </Text>
                     </View>
                 </View>
-            </TouchableHighlight>
-           <HorizontalLine/>
+            </View>
+            <View style={{flex: 1}}>
+                <HorizontalLine/>
+            </View>
         </View>
-
     )
 }
