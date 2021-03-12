ab7bd02209aeb4514230d6e8d098aa6762986323
tout_yong <twotell75@gmail.com>
2021-03-12 22:54:30 +0100

    New button to be able to press a Dish component.

---
+++ source/button/DishButton.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {TouchableHighlight, View} from "react-native";
+import styles_dish from '../styles/styles-dish'
+import all_constants from "../constants";
+import {useColorScheme} from 'react-native-appearance';
+import HorizontalLine from "./HorizontalLine";
+
+
+export default function Dish({...props}) {
+    const colorScheme = useColorScheme();
+    return (
+        <View style={styles_dish.dish_container}>
+            <TouchableHighlight
+                onPress={props.onPress}
+                style={{flex: 1}}
+                underlayColor={all_constants.colors.inputBorderColor}
+            >
+                <Dish
+                    dish_photo={this.getDishPhoto()}
+                    dish_name={this.getDishName()}
+                    dish_category={this.getDishCategory()}
+                    dish_rating={this.getDishRating()}
+                    dish_price={this.getDishPrice()}
+                    dish_description={this.getDishDescription()}
+                    onPress={this.onPress}
+                />
+            </TouchableHighlight>
+           <HorizontalLine/>
+        </View>
+
+    )
+}
