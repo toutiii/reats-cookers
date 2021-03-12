3b20ec4e5460837a04e8143b091f18dc5fdd152e
tout_yong <twotell75@gmail.com>
2021-03-12 23:10:13 +0100

    Dedicated file for styling OrderView.js

---
+++ source/styles/styles-order-view.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from "react-native";
+
+
+let styles_order_view;
+
+export default styles_order_view = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: '25%',
+        marginBottom: '20%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button_container: {
+        flex: 1,
+        margin: '10%',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        width: '95%',
+    }
+})
