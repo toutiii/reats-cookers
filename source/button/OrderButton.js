13b7621f3308e9d3f2d7bafdc02eb88e1de280a8
tout_yong <twotell75@gmail.com>
2021-03-13 03:28:22 +0100

    Integrated FlatList for better rendering.

--- source/button/OrderButton.js
+++ source/button/OrderButton.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {TouchableHighlight, View} from "react-native";
+import {FlatList, TouchableHighlight, View} from "react-native";
 import styles_order from '../styles/styles-order.js'
 import all_constants from "../constants";
 import Order from "../components/Order"
@@ -8,26 +8,33 @@ import HorizontalLine from "../components/HorizontalLine";
 
 export default function OrderButton({...props}) {
     return (
-        <View style={styles_order.order_button_container}>
-            <TouchableHighlight
-                onPress={props.onPress}
-                style={{flex: 1}}
-                underlayColor={all_constants.colors.inputBorderColor}
-            >
-                <Order
-                    order_number={props.order_number}
-                    order_status={props.order_status}
-                    order_owner={props.order_owner}
-                    order_amount={props.order_amount}
-                    order_number_of_items={props.order_number_of_items}
-                    order_date={props.order_date}
-                    order_cancel_date={props.order_cancel_date}
-                    order_delivery_date={props.order_delivery_date}
-                    use_horizontal_line={false}
-                />
-            </TouchableHighlight>
-            <HorizontalLine/>
+        <View>
+            <FlatList
+                data={props.order_list_data}
+                renderItem={({item}) => (
+                    <View style={styles_order.order_button_container}>
+                        <TouchableHighlight
+                            onPress={props.onPress}
+                            style={{flex: 1}}
+                            underlayColor={all_constants.colors.inputBorderColor}
+                        >
+                            <Order
+                                key={item.key}
+                                order_number={item.order_number}
+                                order_status={item.order_status}
+                                order_owner={item.order_owner}
+                                order_amount={item.order_amount}
+                                order_number_of_items={item.order_number_of_items}
+                                order_date={item.order_date}
+                                order_cancel_date={item.order_cancel_date}
+                                order_delivery_date={item.order_delivery_date}
+                                use_horizontal_line={true}
+                                line_width={1}
+                            />
+                        </TouchableHighlight>
+                    </View>
+                )}
+            />
         </View>
-
     )
 }
