68938525566b5cf781f68dfa26b980fb301990e3
tout_yong <twotell75@gmail.com>
2021-03-15 19:02:28 +0100

    First version of top navigation bar for orders.

---
+++ source/tab/OrdersTab.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import PaidOrdersView from "../views/PaidOrdersView";
+import CanceledOrdersView from "../views/CanceledOrdersView";
+import HistoryOrdersView from "../views/HistoryOrdersView";
+import OrderListView from "../views/OrderListView";
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function OrderStack () {
+    return(
+        <Tab.Navigator
+            initialRouteName='Orders'
+            tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'grey',
+                labelStyle: {
+                    fontSize: 12,
+                },
+                style: {
+                    marginTop: '10%'
+                },
+            }}
+        >
+            <Tab.Screen name="Orders" component={OrderListView} options={{ title: 'TOUTES' }}/>
+            <Tab.Screen name="PaidOrders" component={PaidOrdersView} options={{ title: 'PAYÉES' }} />
+            <Tab.Screen name="CanceledOrders" component={CanceledOrdersView} options={{ title: 'ANNULÉES' }} />
+            <Tab.Screen name="HistoryOrders" component={HistoryOrdersView} options={{ title: 'HISTORIQUE' }} />
+        </Tab.Navigator>
+    )
+}
