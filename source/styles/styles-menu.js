7e0b6adafa913ea1f42771468a87c7f2e5e2d3ee
tout_yong <twotell75@gmail.com>
2021-03-17 15:01:15 +0100

    Dedicated style for Menu component.

---
+++ source/styles/styles-menu.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from "react-native";
+
+let styles_menu;
+
+export default styles_menu = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    menu_button_container: {
+        flex: 1,
+        aspectRatio: 16/9,
+        margin: '3%',
+        width: '95%',
+    },
+    dish_label: {
+        fontSize: 16,
+        color: 'tomato',
+    },
+    menu_element: {
+        fontSize: 16
+    },
+})
