0d517f6c57b452d221707f071f474af46e63db59
tout_yong <twotell75@gmail.com>
2021-03-18 04:48:56 +0100

    A flexible enough button to handle SettingsView button infos style.

--- source/button/CustomButton.js
+++ source/button/CustomButton.js
@@ -1,7 +1,10 @@
-import {Text, TouchableHighlight} from "react-native";
+import {Text, TouchableHighlight, View} from "react-native";
 import styles from "../styles/styles";
 import React from "react";
 import all_constants from "../constants";
+import styles_settings from "../styles/styles-settings"
+import Ionicons from "react-native-vector-icons/Ionicons";
+
 
 
 export default function CustomButton ({label, ...props}) {
@@ -16,11 +19,28 @@ export default function CustomButton ({label, ...props}) {
                     {top: props.top},
                     {borderWidth: props.border_width},
                     {borderRadius: props.border_radius},
-                    {backgroundColor: props.backgroundColor? props.backgroundColor : 'transparent'}
+                    {backgroundColor: props.backgroundColor? props.backgroundColor : 'transparent'},
                 ]
             }
         >
-            <Text style={{ color:props.label_color, fontSize: props.font_size }}>{label}</Text>
+            {
+                props.use_second_label ?
+                    <View style={styles_settings.button_container}>
+                        <View style={styles_settings.button_first_label}>
+                            {
+                                props.icon_name ?
+                                    <Ionicons name={props.icon_name} size={30} />
+                                :
+                                    <Text numberOfLines={1} style={{ color:props.label_color, fontSize: props.font_size }}>{label}</Text>
+                            }
+                        </View>
+                        <View style={styles_settings.button_second_label}>
+                            <Text numberOfLines={1} style={{ color:props.label_color, fontSize: props.font_size }}>{props.second_label}</Text>
+                        </View>
+                    </View>
+                :
+                    <Text numberOfLines={1} style={{ color:props.label_color, fontSize: props.font_size }}>{label}</Text>
+            }
         </TouchableHighlight>
     )
 }
