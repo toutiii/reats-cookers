81f61660eab43ab61c9c9f8befdbedd0ac992c09
tout_yong <twotell75@gmail.com>
2021-03-18 22:16:46 +0100

    Unnecessary import when working with Expo.

--- source/App.js
+++ source/App.js
@@ -1,4 +1,3 @@
-import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppearanceProvider } from 'react-native-appearance'
