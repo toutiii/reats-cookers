2b32500b09824377ebe78caa8ba0635eb1f43fe9
tout_yong <twotell75@gmail.com>
2021-03-18 22:18:06 +0100

    First version of Stack Navigator for Home section.

---
+++ source/stack/HomeStack.js
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeView from "../views/HomeView";
+import OrdersTab from "../tab/OrdersTab";
+import BalanceTab from "../tab/BalanceTab";
+
+const Stack = createStackNavigator();
+
+
+export default class HomeNavigator extends Component{
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}
+                initialRouteName='Home'
+            >
+                <Stack.Screen name="Home" component={HomeView} />
+                <Stack.Screen name="OrdersTab" component={OrdersTab} />
+                <Stack.Screen name="BalanceTab" component={BalanceTab} />
+            </Stack.Navigator>
+        )
+    }
+}
