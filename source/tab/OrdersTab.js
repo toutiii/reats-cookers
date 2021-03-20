3077673d567bc4cf267e301b5739be73e27ab0d2
tout_yong <twotell75@gmail.com>
2021-03-20 04:23:33 +0100

    Removed useless imports.

--- source/tab/OrdersTab.js
+++ source/tab/OrdersTab.js
@@ -1,9 +1,5 @@
 import React from "react";
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import OrderPaidStack from "../stack/OrderPaidStack";
-import AllOrderStack from "../stack/AllOrderStack";
-import OrderCancelledStack from "../stack/OrderCancelledStack";
-import OrderHistoryStack from "../stack/OrderHistoryStack";
 import OrderListView from "../listviews/OrderListView";
 import all_constants from "../constants";
 
