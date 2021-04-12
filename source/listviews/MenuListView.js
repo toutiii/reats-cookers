152e73aeea080bba7556c2c2a75769947b386271
tout_yong <twotell75@gmail.com>
2021-04-12 21:40:56 +0200

    Menus data will come from the back end so we simulate that here.

--- source/listviews/MenuListView.js
+++ source/listviews/MenuListView.js
@@ -2,68 +2,14 @@ import React, {Component} from "react";
 import {Alert, View} from "react-native";
 import MenuButton from "../button/MenuButton"
 import styles_menu from "../styles/styles-balance"
+import {getMenus} from "../helpers/menu_helpers";
 
 
 export default class BalanceListView extends Component {
     constructor(props) {
         super(props);
-        this.menu_list_data = [
-            {
-                id :'1',
-                menu_name: 'JOJO',
-                menu_starter: 'Avocats & crevettes',
-                menu_dish: 'Poulet Yassa',
-                menu_dessert: 'Part de moelleux chocolat',
-                menu_price: '13€',
-                menu_comment: 'Menu en promotion !',
-                menu_drink: null,
-            },
-            {
-                id :'2',
-                menu_name: null,
-                menu_starter: null,
-                menu_dish: 'Poulet Yassa',
-                menu_dessert: 'Part de moelleux chocolat',
-                menu_price: '13€',
-                menu_comment: 'Menu en promotion !',
-                menu_drink: null
-            },
-            {
-                id :'3',
-                menu_name: null,
-                menu_starter: 'Avocats & crevettes',
-                menu_dish: 'Poulet Yassa',
-                menu_dessert: null,
-                menu_price: '13€',
-                menu_comment: 'Menu en promotion !',
-                menu_drink: 'Fanta',
-            },
-            {
-                id :'4',
-                menu_name: null,
-                menu_starter: 'Avocats & crevettes',
-                menu_dish: 'Poulet Yassa',
-                menu_dessert: 'Part de moelleux chocolat',
-                menu_price: '13€',
-                menu_comment: 'Menu en promotion !',
-                menu_drink: null,
-            },
-            {
-                id :'5',
-                menu_name: null,
-                menu_starter: 'Avocats & crevettes',
-                menu_dish: 'Poulet Yassa',
-                menu_dessert: 'Part de moelleux chocolat',
-                menu_price: '13€',
-                menu_comment: 'Menu en promotion !',
-                menu_drink: 'Coca',
-            },
-        ]
     }
 
-    getMenus = () => {
-        return this.menu_list_data
-    }
     onPress = () => {
         Alert.alert('ZA WARUDO')
     }
@@ -71,7 +17,7 @@ export default class BalanceListView extends Component {
         return(
             <View style={styles_menu.container}>
                 <MenuButton
-                    menu_list_data={this.getMenus()}
+                    menu_list_data={getMenus()}
                     onPress={this.onPress}
                 />
             </View>
