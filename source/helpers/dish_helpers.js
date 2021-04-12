d9a1eaa8f2724767106af75cc9726dca3b5721ee
tout_yong <twotell75@gmail.com>
2021-04-12 20:47:44 +0200

    Toolbox for Dishes management.

---
+++ source/helpers/dish_helpers.js
@@ -0,0 +1,70 @@
+export function getDishCategories() {
+    const dish_categories = [
+        { label: 'Entrée', value: 'starter'},
+        { label: 'Plat principal', value: 'main_dish' },
+        { label: 'Boisson', value: 'drink' },
+        { label: 'Dessert', value: 'dessert' },
+    ]
+    return dish_categories
+}
+
+
+export function getDishes() {
+    const dish_list_data = [
+        {
+            id: '1',
+            dish_photo: 'https://img-3.journaldesfemmes.fr/M_bbWpTVNekL5O_MLzQ4dyInmJU=/750x/smart/1c9fe4d4419047f18efc37134a046e5a/recipe-jdf/1001383.jpg',
+            dish_name: 'Poulet Yassa DU FUTUR',
+            dish_category: 'starter',
+            dish_rating: '4.8/5',
+            dish_price: '200',
+            dish_description: 'Un succulent poulet Yassa',
+        },
+        {
+            id: '2',
+            dish_photo: 'https://img-3.journaldesfemmes.fr/M_bbWpTVNekL5O_MLzQ4dyInmJU=/750x/smart/1c9fe4d4419047f18efc37134a046e5a/recipe-jdf/1001383.jpg',
+            dish_name: 'Poulet Yassa 11',
+            dish_category: 'dessert',
+            dish_rating: '4.8/5',
+            dish_price: '11.5',
+            dish_description: 'Un succulent poulet Yassa 11',
+        },
+        {
+            id: '3',
+            dish_photo: 'https://img-3.journaldesfemmes.fr/M_bbWpTVNekL5O_MLzQ4dyInmJU=/750x/smart/1c9fe4d4419047f18efc37134a046e5a/recipe-jdf/1001383.jpg',
+            dish_name: 'Poulet Yassa 111',
+            dish_category: 'main_dish',
+            dish_rating: '4.8/5',
+            dish_price: '9.5',
+            dish_description: 'Un succulent poulet Yassa 111',
+        },
+        {
+            id: '4',
+            dish_photo: 'https://img-3.journaldesfemmes.fr/M_bbWpTVNekL5O_MLzQ4dyInmJU=/750x/smart/1c9fe4d4419047f18efc37134a046e5a/recipe-jdf/1001383.jpg',
+            dish_name: 'Poulet Yassa 1111',
+            dish_category: 'main_dish',
+            dish_rating: '4.8/5',
+            dish_price: '12',
+            dish_description: 'Un succulent poulet Yassa 1111',
+        },
+        {
+            id: '5',
+            dish_photo: 'https://img-3.journaldesfemmes.fr/M_bbWpTVNekL5O_MLzQ4dyInmJU=/750x/smart/1c9fe4d4419047f18efc37134a046e5a/recipe-jdf/1001383.jpg',
+            dish_name: 'Poulet Yassa 11111',
+            dish_category: 'main_dish',
+            dish_rating: '4.8/5',
+            dish_price: '8',
+            dish_description: 'Un succulent poulet Yassa 11111',
+        },
+        {
+            id: '6',
+            dish_photo: 'https://img-3.journaldesfemmes.fr/M_bbWpTVNekL5O_MLzQ4dyInmJU=/750x/smart/1c9fe4d4419047f18efc37134a046e5a/recipe-jdf/1001383.jpg',
+            dish_name: 'Poulet Yassa 111111',
+            dish_category: 'main_dish',
+            dish_rating: '4.8/5',
+            dish_price: '7',
+            dish_description: 'Un succulent poulet Yassa 111111',
+        },
+    ]
+    return dish_list_data
+}
