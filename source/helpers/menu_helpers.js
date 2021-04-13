3feed10436eaadce202b9c4335869b8a21e58da1
tout_yong <twotell75@gmail.com>
2021-04-13 16:50:06 +0200

    Toolbox of functions used by Menu section.

---
+++ source/helpers/menu_helpers.js
@@ -0,0 +1,55 @@
+export function getMenus() {
+    const menu_list_data = [
+        {
+            id :'1',
+            menu_name: 'JOJO',
+            menu_starter: 'Avocats & crevettes',
+            menu_main_dish: 'Poulet Yassa',
+            menu_dessert: 'Part de moelleux chocolat',
+            menu_price: '13',
+            menu_comment: 'Menu en promotion !',
+            menu_drink: null,
+        },
+        {
+            id :'2',
+            menu_name: null,
+            menu_starter: null,
+            menu_main_dish: 'Poulet Yassa',
+            menu_dessert: 'Part de moelleux chocolat',
+            menu_price: '13',
+            menu_comment: 'Menu en promotion !',
+            menu_drink: null
+        },
+        {
+            id :'3',
+            menu_name: null,
+            menu_starter: 'Avocats & crevettes',
+            menu_main_dish: 'Poulet Yassa',
+            menu_dessert: null,
+            menu_price: '13',
+            menu_comment: 'Menu en promotion !',
+            menu_drink: 'Fanta',
+        },
+        {
+            id :'4',
+            menu_name: null,
+            menu_starter: 'Avocats & crevettes',
+            menu_main_dish: 'Poulet Yassa',
+            menu_dessert: 'Part de moelleux chocolat',
+            menu_price: '13',
+            menu_comment: 'Menu en promotion !',
+            menu_drink: null,
+        },
+        {
+            id :'5',
+            menu_name: null,
+            menu_starter: 'Avocats & crevettes',
+            menu_main_dish: 'Poulet Yassa',
+            menu_dessert: 'Part de moelleux chocolat',
+            menu_price: '13',
+            menu_comment: 'Menu en promotion !',
+            menu_drink: 'Coca',
+        },
+    ]
+    return menu_list_data;
+}
