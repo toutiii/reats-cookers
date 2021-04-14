535b34175ab095ac8c020d5c0f8df33b006b43ab
tout_yong <twotell75@gmail.com>
2021-04-14 18:56:39 +0200

    New toolbox useful for user settings management.

---
+++ source/helpers/settings_helpers.js
@@ -0,0 +1,15 @@
+export function getUserSettings() {
+    const userSettingsObject = {
+        'siret': 'FJGUT2-FRF-55',
+        'firstname': 'Irène',
+        'lastname': 'JANTEN',
+        'phone': '0649510110',
+        'address': "1 rue René Cassin",
+        'postal_code': '91100',
+        'town': "Corbeil-Essonnes",
+        'order_days': 'Lundi, Mercredi, Vendredi',
+        'delivery_days': 'Mardi, Jeudi, Samedi',
+        'max_order_number': 30,
+    }
+    return userSettingsObject
+}
