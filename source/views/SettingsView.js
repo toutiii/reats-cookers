3c6b1f587b77e54f3ea4a45869bead62691b3324
tout_yong <twotell75@gmail.com>
2021-04-14 18:52:16 +0200

    A new Setting component allows us to be DRY and reduce code.

--- source/views/SettingsView.js
+++ source/views/SettingsView.js
@@ -4,6 +4,7 @@ import all_constants from "../constants";
 import styles_settings from "../styles/styles-settings"
 import CustomButton from "../button/CustomButton";
 import {getUserSettings} from "../helpers/settings_helpers";
+import Setting from "../components/Setting"
 
 
 export default class SettingsView extends Component {
@@ -20,62 +21,48 @@ export default class SettingsView extends Component {
                 <View style={styles_settings.title}>
                     <Text style={styles_settings.title_text}> {all_constants.label.settings.my_account} </Text>
                 </View>
-                <View style={{flex: 12}}>
-                    <View style={{flex: 1, justifyContent: 'center'}}>
-                        <CustomButton
-                            label={all_constants.label.settings.siret}
-                            height={50}
-                            font_size={20}
-                            use_second_label={true}
-                            second_label={this.userInfosObject.siret}
-                            borderBottomWidth={1}
-                        />
-                    </View>
-                    <View style={{flex: 1, justifyContent: 'center'}}>
-                        <CustomButton
-                            label={all_constants.label.settings.firstname}
-                            height={50}
-                            font_size={20}
-                            use_second_label={true}
-                            second_label={this.userInfosObject.firstname}
-                            onPress={this.onSubmit}
-                            borderBottomWidth={1}
-                        />
-                    </View>
-                    <View style={{flex: 1, justifyContent: 'center'}}>
-                        <CustomButton
-                            label={all_constants.label.settings.lastname}
-                            height={50}
-                            font_size={20}
-                            use_second_label={true}
-                            second_label={this.userInfosObject.lastname}
-                            onPress={this.onSubmit}
-                            borderBottomWidth={1}
-                        />
-                    </View>
-                    <View style={{flex: 1, justifyContent: 'center'}}>
-                        <CustomButton
-                            icon_name='location'
-                            height={50}
-                            font_size={20}
-                            use_second_label={true}
-                            second_label={this.userInfosObject.address}
-                            onPress={this.onSubmit}
-                            borderBottomWidth={1}
-                        />
-                    </View>
-                    <View style={{flex: 1, justifyContent: 'center'}}>
-                        <CustomButton
-                            label={all_constants.label.settings.postal_code}
-                            height={50}
-                            font_size={20}
-                            use_second_label={true}
-                            second_label={this.userInfosObject.postal_code}
-                            onPress={this.onSubmit}
-                            borderBottomWidth={1}
-                        />
-                    </View>
-                    <View style={{flex: 1, justifyContent: 'center'}}>
+                <View style={{flex: 7}}>
+                    <Setting
+                        label={all_constants.label.settings.siret}
+                        value={this.userInfosObject.siret}
+                    />
+                    <Setting
+                        label={all_constants.label.settings.firstname}
+                        value={this.userInfosObject.firstname}
+                    />
+                    <Setting
+                        label={all_constants.label.settings.lastname}
+                        value={this.userInfosObject.lastname}
+                    />
+                    <Setting
+                        icon_name='location'
+                        value={this.userInfosObject.address}
+                    />
+                    <Setting
+                        label={all_constants.label.settings.postal_code}
+                        value={this.userInfosObject.postal_code}
+                    />
+                    <Setting
+                        label={all_constants.label.settings.town}
+                        value={this.userInfosObject.town}
+                    />
+                    <Setting
+                        icon_name='call'
+                        value={this.userInfosObject.phone}
+                    />
+                    <Setting
+                        label={all_constants.label.settings.order_days}
+                        value={this.userInfosObject.order_days}
+                    />
+                    <Setting
+                        label={all_constants.label.settings.delivery_days}
+                        value={this.userInfosObject.delivery_days}
+                    />
+                    <Setting
+                        label={all_constants.label.settings.max_order_number}
+                        value={this.userInfosObject.max_order_number}
+                    />
+                </View>
                         <CustomButton
                             label={all_constants.label.settings.town}
                             height={50}
