144f380a1a22e5711e5c0c477d27e83bc43cd1ee
tout_yong <twotell75@gmail.com>
2021-04-15 14:58:49 +0200

    We do not use Ionicons anymore.

--- source/components/Setting.js
+++ source/components/Setting.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {ScrollView, Text, View} from "react-native";
 import HorizontalLine from "./HorizontalLine";
-import Ionicons from "react-native-vector-icons/Ionicons";
 
 
 export default function Setting({...props}) {
@@ -9,14 +8,9 @@ export default function Setting({...props}) {
         <View style={{flex: 1, justifyContent: 'center', width: '95%'}}>
             <View style={{flex: 1, flexDirection: 'row'}}>
                 <View style={{flex: 1, justifyContent: 'center', marginTop: '5%'}}>
-                    {
-                        props.icon_name ?
-                            <Ionicons name={props.icon_name} size={30} />
-                        :
-                            <ScrollView horizontal={true}>
-                                <Text numberOfLines={1} style={{fontSize: 18}}>{props.label}</Text>
-                            </ScrollView>
-                    }
+                    <ScrollView horizontal={true}>
+                        <Text numberOfLines={1} style={{fontSize: 18}}>{props.label}</Text>
+                    </ScrollView>
                 </View>
                 <View style={{flex: 1, justifyContent: 'center', alignItems: 'flex-end', marginTop: '5%'}}>
                     <ScrollView horizontal={true}>
