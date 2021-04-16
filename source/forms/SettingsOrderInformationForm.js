431d7066b095f06d6d1d1c6b8dbc8054e2b0c3bb
tout_yong <twotell75@gmail.com>
2021-04-16 10:38:22 +0200

    First version of SettingsOrderInformationForm to update user order preferences.

---
+++ source/forms/SettingsOrderInformationForm.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Form from "./Form";
+import all_constants from "../constants";
+import {View} from "react-native";
+import update_user_settings from "../api/update_settings";
+
+
+export default function SettingsOrderInformationForm ({...props}){
+    const handleResult = async (result) => {
+        if (result.ok) {
+            props.navigation.goBack(null);
+        } else {
+            throw new Error('Failed.');
+        }
+    };
+
+    return(
+        <View style={{flex: 1}}>
+            <View style={{flex: 2, marginTop: '10%'}}>
+                <Form
+                    action={update_user_settings}
+                    navigation={props.navigation}
+                    afterSubmit={handleResult}
+                    item={props.route.params.item}
+                    fields={{
+                        user_settings_order_days: {
+                            type: all_constants.field_type.textinput,
+                            label: all_constants.label.form.settings.order_days,
+                            validators: [],
+                            maxLength: 10,
+                        },
+                        user_settings_delivery_days: {
+                            type: all_constants.field_type.textinput,
+                            label: all_constants.label.form.settings.delivery_days,
+                            validators: [],
+                            maxLength: 50,
+                        },
+                        user_settings_max_order_number: {
+                            type: all_constants.field_type.textinput,
+                            label: all_constants.label.form.settings.max_order_number,
+                            validators: [],
+                            maxLength: 50,
+                        },
+                    }}
+                />
+            </View>
+        </View>
+    )
+}
