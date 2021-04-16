036c0dbf7e357facdfa54ab6dfdc0c09dfcfd8cb
tout_yong <twotell75@gmail.com>
2021-04-16 10:35:49 +0200

    First version of SettingsCredentialsForm to update credentials.

---
+++ source/forms/SettingsCredentialsForm.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Form from "./Form";
+import all_constants from "../constants";
+import {View} from "react-native";
+import update_user_settings from "../api/update_settings";
+
+
+export default function SettingsCredentialsForm ({...props}){
+    const handleResult = async (result) => {
+        if (result.ok) {
+            props.navigation.goBack(null);
+        } else {
+            throw new Error('Failed.');
+        }
+    };
+
+    return(
+        <View style={{flex: 1}}>
+            <View style={{flex: 2, marginTop: '10%'}}>
+                <Form
+                    action={update_user_settings}
+                    navigation={props.navigation}
+                    afterSubmit={handleResult}
+                    item={props.route.params.item}
+                    fields={{
+                        user_settings_email: {
+                            type: all_constants.field_type.textinput,
+                            label: all_constants.label.form.settings.email,
+                            validators: [],
+                            maxLength: 100,
+                        },
+                        user_settings_current_password: {
+                            type: all_constants.field_type.textinput,
+                            label: all_constants.label.form.settings.current_password,
+                            validators: [],
+                            maxLength: 10,
+                        },
+                        user_settings_new_password: {
+                            type: all_constants.field_type.textinput,
+                            label: all_constants.label.form.settings.new_password,
+                            validators: [],
+                            maxLength: 10,
+                        },
+                        user_settings_new_password_confirmation: {
+                            type: all_constants.field_type.textinput,
+                            label: all_constants.label.form.settings.new_password_confirmation,
+                            validators: [],
+                            maxLength: 10,
+                        },
+                    }}
+                />
+            </View>
+        </View>
+    )
+}
