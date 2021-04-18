71aec9a4a698e2c65fc785656f6158e874550fa8
tout_yong <twotell75@gmail.com>
2021-04-18 23:00:26 +0200

    User may want to remove or just disable a menu without removing it.

--- source/forms/MenuFormView.js
+++ source/forms/MenuFormView.js
@@ -22,6 +22,8 @@ export default function MenuFormView ({...props}){
                     navigation={props.navigation}
                     afterSubmit={handleResult}
                     item={props.route.params.item}
+                    third_button_label={all_constants.label.dishes.disable_dish}
+                    fourth_button_label={all_constants.label.dishes.remove_dish}
                     fields={{
                         menu_name: {
                             type: all_constants.field_type.textinput,
