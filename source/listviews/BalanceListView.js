cd63f73b92c17d1495f1642b7b6a2117e43f1380
tout_yong <twotell75@gmail.com>
2021-04-21 21:36:00 +0200

    The data will be send by the backend through balance_helper.

--- source/listviews/BalanceListView.js
+++ source/listviews/BalanceListView.js
@@ -8,50 +8,6 @@ import all_constants from "../constants";
 export default class BalanceListView extends Component {
     constructor(props) {
         super(props);
-        this.balance_list_data = [
-            {
-                id :'1',
-                order_number: '123365',
-                order_amount: '50€',
-            },
-            {
-                id :'2',
-                order_number: '123365',
-                order_amount: '50€',
-            },
-            {
-                id :'3',
-                order_number: '123365',
-                order_amount: '50€',
-            },
-            {
-                id :'4',
-                order_number: '123365',
-                order_amount: '50€',
-            },
-            {
-                id :'5',
-                order_number: '123365',
-                order_amount: '50€',
-            },
-            {
-                id :'6',
-                order_number: '123365',
-                order_amount: '50€',
-            },
-            {
-                id :'7',
-                order_number: '123365',
-                order_amount: '50€',
-            },
-            {
-                id :'8',
-                order_number: '123365',
-                order_amount: '50€',
-            },
-        ]
-    }
-
     getData = () => {
         if (this.props.route.params.tag === all_constants.tag.balance.pending) {
             return this.balance_list_data
