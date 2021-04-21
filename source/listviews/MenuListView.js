0bfa5ad5ee98f998b82731fcd680a1ba4d0234ff
tout_yong <twotell75@gmail.com>
2021-04-21 09:18:36 +0200

    To separate enabled menu from disabled ones.

--- source/listviews/MenuListView.js
+++ source/listviews/MenuListView.js
@@ -10,11 +10,27 @@ export default class MenuListView extends Component {
         super(props);
     }
 
+    getData = () => {
+        const allMenus = getMenus()
+        let menuData = []
+        const indexes = Object.keys(allMenus)
+        for (let i = 1; i <= indexes.length; i++) {
+            const menuObject = allMenus[i]
+            if (
+                menuObject['dish_category'] === this.props.route.params.tag
+                &&
+                menuObject['isEnabled'] === this.props.route.params.isEnabled
+            ){
+                menuData.push(menuObject)
+            }
+        }
+        return menuData
+    }
     render() {
         return(
             <View style={styles_menu.container}>
                 <MenuButton
-                    menu_list_data={getMenus()}
+                    menu_list_data={this.getData()}
                     allProps={this.props}
                 />
             </View>
