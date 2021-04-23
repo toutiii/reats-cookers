7f3d302ee9a731fb9ff1244b650a041ef4d73ef0
tout_yong <twotell75@gmail.com>
2021-04-23 12:01:56 +0200

    Again, to have JSON valid format.

--- source/helpers/menu_helpers.js
+++ source/helpers/menu_helpers.js
@@ -1,60 +1,62 @@
 export function getMenus() {
     const menu_list_data = {
-        1:{
-            id :'1',
-            menu_name: 'JOJO',
-            menu_starter: 'Avocats & crevettes',
-            menu_main_dish: 'Poulet Yassa',
-            menu_dessert: 'Part de moelleux chocolat',
-            menu_price: '13',
-            menu_comment: 'Menu en promotion !',
-            menu_drink: null,
-            isEnabled: false,
-        },
-        2:{
-            id :'2',
-            menu_name: null,
-            menu_starter: null,
-            menu_main_dish: 'Poulet Yassa',
-            menu_dessert: 'Part de moelleux chocolat',
-            menu_price: '13',
-            menu_comment: 'Menu en promotion !',
-            menu_drink: null,
-            isEnabled: false,
-        },
-        3:{
-            id :'3',
-            menu_name: null,
-            menu_starter: 'Avocats & crevettes',
-            menu_main_dish: 'Poulet Yassa',
-            menu_dessert: null,
-            menu_price: '13',
-            menu_comment: 'Menu en promotion !',
-            menu_drink: 'Fanta',
-            isEnabled: false,
-        },
-        4:{
-            id :'4',
-            menu_name: null,
-            menu_starter: 'Avocats & crevettes',
-            menu_main_dish: 'Poulet Yassa',
-            menu_dessert: 'Part de moelleux chocolat',
-            menu_price: '13',
-            menu_comment: 'Menu en promotion !',
-            menu_drink: null,
-            isEnabled: true,
-        },
-        5:{
-            id :'5',
-            menu_name: null,
-            menu_starter: 'Avocats & crevettes',
-            menu_main_dish: 'Poulet Yassa',
-            menu_dessert: 'Part de moelleux chocolat',
-            menu_price: '13',
-            menu_comment: 'Menu en promotion !',
-            menu_drink: 'Coca',
-            isEnabled: true,
-        },
+        "data": [
+            {
+                "id" : "1",
+                "menu_name": "JOJO",
+                "menu_starter": "Avocats & crevettes",
+                "menu_main_dish": "Poulet Yassa",
+                "menu_dessert": "Part de moelleux chocolat",
+                "menu_price": "13",
+                "menu_comment": "Menu en promotion !",
+                "menu_drink": null,
+                "isEnabled": false
+            },
+            {
+                "id" : "2",
+                "menu_name": null,
+                "menu_starter": null,
+                "menu_main_dish": "Poulet Yassa",
+                "menu_dessert": "Part de moelleux chocolat",
+                "menu_price": "13",
+                "menu_comment": "Menu en promotion !",
+                "menu_drink": null,
+                "isEnabled": false
+            },
+            {
+                "id" : "3",
+                "menu_name": null,
+                "menu_starter": "Avocats & crevettes",
+                "menu_main_dish": "Poulet Yassa",
+                "menu_dessert": null,
+                "menu_price": "13",
+                "menu_comment": "Menu en promotion !",
+                "menu_drink": "Fanta",
+                "isEnabled": false
+            },
+            {
+                "id" : "4",
+                "menu_name": null,
+                "menu_starter": "Avocats & crevettes",
+                "menu_main_dish": "Poulet Yassa",
+                "menu_dessert": "Part de moelleux chocolat",
+                "menu_price": "13",
+                "menu_comment": "Menu en promotion !",
+                "menu_drink": null,
+                "isEnabled": true
+            },
+            {
+                "id" : "5",
+                "menu_name": null,
+                "menu_starter": "Avocats & crevettes",
+                "menu_main_dish": "Poulet Yassa",
+                "menu_dessert": "Part de moelleux chocolat",
+                "menu_price": "13",
+                "menu_comment": "Menu en promotion !",
+                "menu_drink": "Coca",
+                "isEnabled": true
+            }
+        ]
     }
     return menu_list_data;
 }
