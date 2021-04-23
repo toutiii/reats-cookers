4c3be8a21f22bdc684ea9f6eb40c1842c63b5a97
tout_yong <twotell75@gmail.com>
2021-04-23 09:33:24 +0200

    The previous format was not a valid JSON.

--- source/helpers/balance_helpers.js
+++ source/helpers/balance_helpers.js
@@ -1,45 +1,47 @@
 export function getBalanceData () {
     const balance_list_data = {
-        1:{
-            id :'1',
-            order_number: '123365',
-            order_amount: '50€',
-        },
-        2:{
-            id :'2',
-            order_number: '123365',
-            order_amount: '50€',
-        },
-        3:{
-            id :'3',
-            order_number: '123365',
-            order_amount: '50€',
-        },
-        4:{
-            id :'4',
-            order_number: '123365',
-            order_amount: '50€',
-        },
-        5:{
-            id :'5',
-            order_number: '123365',
-            order_amount: '50€',
-        },
-        6:{
-            id :'6',
-            order_number: '123365',
-            order_amount: '50€',
-        },
-        7:{
-            id :'7',
-            order_number: '123365',
-            order_amount: '50€',
-        },
-        8:{
-            id :'8',
-            order_number: '123365',
-            order_amount: '50€',
-        },
+        "data": [
+            {
+                "id": "1",
+                "order_number": "123365",
+                "order_amount": "50€"
+            },
+            {
+                "id": "2",
+                "order_number": "123365",
+                "order_amount": "50€"
+            },
+            {
+                "id": "3",
+                "order_number": "123365",
+                "order_amount": "50€"
+            },
+            {
+                "id": "4",
+                "order_number": "123365",
+                "order_amount": "50€"
+            },
+            {
+                "id": "5",
+                "order_number": "123365",
+                "order_amount": "50€"
+            },
+            {
+                "id": "6",
+                "order_number": "123365",
+                "order_amount": "50€"
+            },
+            {
+                "id": "7",
+                "order_number": "123365",
+                "order_amount": "50€"
+            },
+            {
+                "id": "8",
+                "order_number": "123365",
+                "order_amount": "50€"
+            }
+        ]
     }
     let data = []
     const indexes = Object.keys(balance_list_data)
