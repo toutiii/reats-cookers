8db4b1b0b80b3846169a7302364bbbf83b0e2766
tout_yong <twotell75@gmail.com>
2021-04-26 18:56:42 +0200

    Those function are not specific to dishes form.

--- source/validators/validate_dishformview.js
+++ source/validators/validate_dishformview.js
@@ -18,34 +18,3 @@ export function validateDescriptionLength(description) {
         return 'Too long description( ' + all_constants.validators.max_text_length + 'max)';
     }
 };
-
-export function validateFields(fields, values) {
-    const errors = {};
-    const fieldNameKeys = Object.keys(fields)
-    fieldNameKeys.forEach((fieldName) => {
-        const fieldObject = fields[fieldName];
-        const validators = fieldObject.validators;
-        const fieldLabel = fieldObject.label;
-        const value = values[fieldName];
-        if (validators && validators.length > 0) {
-            const error = validateField(validators, value, fieldLabel);
-            if (error) {
-                errors[fieldName] = error;
-            }
-        }
-    });
-    return errors;
-};
-
-export function validateField(validators, value, fieldLabel) {
-    let error = '';
-    validators.forEach((validator) => {
-        if (typeof validator !== 'undefined') {
-            const validationError = validator(value, fieldLabel);
-            if (validationError) {
-                error = validationError;
-            }
-        }
-    });
-    return error;
-};
