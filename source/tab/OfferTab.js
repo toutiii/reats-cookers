954247820a47624e1df2300765705cc6fe2ad2af
tout_yong <twotell75@gmail.com>
2021-05-03 22:21:08 +0200

    New tab for special offer section.

---
+++ source/tab/OfferTab.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import OfferListView from "../listviews/OfferListView";
+import all_constants from "../constants";
+import StateTab from "./StateTab";
+import OfferFormView from "../forms/OfferFormView";
+
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function OfferTab () {
+    return(
+        <Tab.Navigator
+            initialRouteName='Offers'
+            tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'grey',
+                labelStyle: {
+                    fontSize: 12,
+                },
+                style: {
+                    marginTop: '7%'
+                },
+            }}
+        >
+            <Tab.Screen
+                name="Offers"
+                component={StateTab}
+                options={{ title: all_constants.tab.offer_tab.title.offer}}
+                initialParams={{
+                    tag: all_constants.tag.offer.promotion,
+                    child_component: OfferListView
+                }}
+            />
+            <Tab.Screen
+                name="Add"
+                component={OfferFormView}
+                options={{ title: all_constants.tab.dish_tab.title.add_item}}
+                initialParams={{item: {}}}
+            />
+        </Tab.Navigator>
+    )
+}
