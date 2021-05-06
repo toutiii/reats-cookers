fd9d23ddc3e9b17c837caa65280231f0722d538b
tout_yong <twotell75@gmail.com>
2021-05-06 03:44:22 +0200

    We need it to be able to print order content.

--- source/views/OrderView.js
+++ source/views/OrderView.js
@@ -46,6 +46,7 @@ export default class OrderView extends Component {
                         order_cancel_date={this.props.route.params.item.order_cancel_date}
                         order_delivery_date={this.props.route.params.item.order_delivery_date}
                         order_number_color={this.props.route.params.item.order_number_color}
+                        order_dishes={this.props.route.params.item.dishes}
                     />
                     <HorizontalLine
                         line_width={3}
