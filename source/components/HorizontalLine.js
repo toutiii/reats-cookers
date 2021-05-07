fe221f2bc2f6c7d72124d2727c1bfa57cd2d118e
tout_yong <twotell75@gmail.com>
2021-05-07 06:10:20 +0200

    In case we want to specify an other color than the default.

--- source/components/HorizontalLine.js
+++ source/components/HorizontalLine.js
@@ -5,7 +5,10 @@ import {View} from "react-native";
 export default function HorizontalLine({...props}){
     return (
         <View
-            style={{borderBottomWidth: props.line_width ? props.line_width : 1}}
+            style={[
+                {borderBottomWidth: props.line_width ? props.line_width : 1},
+                {borderBottomColor: props.line_color ? props.line_color: 'black'}
+            ]}
         />
     )
 }
