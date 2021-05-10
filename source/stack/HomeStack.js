ddc1005535cc3ad7b046afb3a3562e7bf0c044fc
tout_yong <twotell75@gmail.com>
2021-05-10 18:30:55 +0200

    To avoid harcode string directly in view.

--- source/stack/HomeStack.js
+++ source/stack/HomeStack.js
@@ -4,6 +4,7 @@ import HomeView from "../views/HomeView";
 import OrdersTab from "../tab/OrdersTab";
 import BalanceTab from "../tab/BalanceTab";
 import SettingsView from "../views/SettingsView";
+import all_constants from "../constants";
 
 const Stack = createStackNavigator();
 
@@ -23,7 +24,11 @@ export default class HomeStack extends Component{
                 <Stack.Screen name="Home" component={HomeView} options={{headerShown: false}} />
                 <Stack.Screen name="OrdersTab" component={OrdersTab} />
                 <Stack.Screen name="BalanceTab" component={BalanceTab} />
-                <Stack.Screen name="Settings" component={SettingsView} options={{ title: 'Mes Paramètres' }}/>
+                <Stack.Screen
+                    name="Settings"
+                    component={SettingsView}
+                    options={{ title: all_constants.label.settings.my_account}}
+                />
             </Stack.Navigator>
         )
     }
