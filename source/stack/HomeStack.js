dda378b6c642e1e4285fe9352a8a9bdc102d06ae
tout_yong <twotell75@gmail.com>
2021-05-10 18:23:03 +0200

    To not be stucked in SettingsView if go back device button not working.

--- source/stack/HomeStack.js
+++ source/stack/HomeStack.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeView from "../views/HomeView";
 import OrdersTab from "../tab/OrdersTab";
 import BalanceTab from "../tab/BalanceTab";
+import SettingsView from "../views/SettingsView";
 
 const Stack = createStackNavigator();
 
@@ -22,6 +23,7 @@ export default class HomeStack extends Component{
                 <Stack.Screen name="Home" component={HomeView} options={{headerShown: false}} />
                 <Stack.Screen name="OrdersTab" component={OrdersTab} />
                 <Stack.Screen name="BalanceTab" component={BalanceTab} />
+                <Stack.Screen name="Settings" component={SettingsView} options={{ title: 'Mes Paramètres' }}/>
             </Stack.Navigator>
         )
     }
