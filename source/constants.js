97b6e91210704fc6e1d2bba1100fcc4766d1e477
tout_yong <twotell75@gmail.com>
2021-05-12 00:22:32 +0200

    To not harcode approved status in views.

--- source/constants.js
+++ source/constants.js
@@ -181,6 +181,7 @@ export const all_constants = {
             canceled: 'Annulée',
             delivered: 'Livrée',
             pending: "En attente de prise en charge",
+            approved: "Acceptée"
         },
         no_order_found: 'No orders found.',
     },
