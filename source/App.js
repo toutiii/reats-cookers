9a9057246ce4a33caa10722063b9398555a9345b
tout_yong <twotell75@gmail.com>
2021-05-16 21:33:46 +0200

    When user run the app we do not want to show all tabs as user has to sign in first.

--- source/App.js
+++ source/App.js
@@ -18,73 +18,29 @@ import MenuTab from "./tab/MenuTab";
 import OfferTab from "./tab/OfferTab";
 import OfferFormView from "./forms/OfferFormView";
 import AddView from "./views/AddView";
+import LoginFormView from "./forms/LoginFormView";
+import {createStackNavigator} from "@react-navigation/stack";
+import HomeView from "./views/HomeView";
+import all_constants from "./constants";
+import SettingsView from "./views/SettingsView";
+import MainTabNavigator from "./tab/MainTabNavigator";
 
-const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export default class App extends Component {
     render () {
         return (
             <AppearanceProvider>
                 <NavigationContainer>
-                    <Tab.Navigator
-                        initialRouteName="Home"
-                        screenOptions={({ route }) => ({
-                            tabBarIcon: ({ focused, color, size }) => {
-                                let iconName;
-                                if (route.name === 'Home') {
-                                    iconName = 'home-outline'
-                                }else if (route.name === 'Orders') {
-                                    iconName = 'basket-outline';
-                                }else if (route.name === 'Dishes') {
-                                    iconName = 'restaurant-outline';
-                                }else if (route.name === 'Menus') {
-                                    iconName = 'book-outline';
-                                }else if (route.name === 'Offers') {
-                                    iconName = 'pricetag-outline';
-                                }else if (route.name === 'Add') {
-                                    iconName = 'add-circle-outline';
-                                }else if (route.name === 'Balance') {
-                                    iconName = 'cash-outline';
-                                }
-                                // You can return any component that you like here!
-                                return <Ionicons name={iconName} size={size} color={color} />;
-                            },
-                            tabBarButton: [
-                                "OrderView",
-                                "DishFormView",
-                                "MenuFormView",
-                                "SettingsCredentialsForm",
-                                "SettingsPersonalInformationForm",
-                                "SettingsAddressForm",
-                                "SettingsOrderInformationForm",
-                                "Balance",
-                                "OfferFormView",
-                            ].includes(route.name)
-                                ? () => {
-                                    return null;
-                                }
-                                : undefined,
-                        })}
-                        tabBarOptions={{
-                            activeTintColor: 'tomato',
-                            inactiveTintColor: 'gray',
+                    <Stack.Navigator
+                        screenOptions={{
+                            headerShown: false
                         }}
+                        initialRouteName='Signin'
                     >
-                        <Tab.Screen name="Home" component={HomeStack} />
-                        <Tab.Screen name="Orders" component={OrdersTab} options={{ tabBarBadge: 3 }} />
-                        <Tab.Screen name="Dishes" component={DishTab} />
-                        <Tab.Screen name="Menus" component={MenuTab} />
-                        <Tab.Screen name="Offers" component={OfferTab} />
-                        <Tab.Screen name="Add" component={AddView} />
-                        <Tab.Screen name="OrderView" component={OrderView}/>
-                        <Tab.Screen name="DishFormView" component={DishFormView}/>
-                        <Tab.Screen name="MenuFormView" component={MenuFormView}/>
-                        <Tab.Screen name="OfferFormView" component={OfferFormView}/>
-                        <Tab.Screen name="SettingsCredentialsForm" component={SettingsCredentialsForm}/>
-                        <Tab.Screen name="SettingsPersonalInformationForm" component={SettingsPersonalInformationForm}/>
-                        <Tab.Screen name="SettingsAddressForm" component={SettingsAddressForm}/>
-                        <Tab.Screen name="SettingsOrderInformationForm" component={SettingsOrderInformationForm}/>
-                    </Tab.Navigator>
+                        <Stack.Screen name="Signin" component={LoginFormView} />
+                        <Stack.Screen name="MainTabNavigator" component={MainTabNavigator} />
+                    </Stack.Navigator>
                 </NavigationContainer>
             </AppearanceProvider>
         )
