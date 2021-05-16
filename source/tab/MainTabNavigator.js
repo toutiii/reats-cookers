2bef4d9800c722f0b2261b4b601e79404cdede14
tout_yong <twotell75@gmail.com>
2021-05-16 22:25:46 +0200

    To not overload App.js

---
+++ source/tab/MainTabNavigator.js
@@ -0,0 +1,83 @@
+import Ionicons from "react-native-vector-icons/Ionicons";
+import HomeStack from "../stack/HomeStack";
+import OrdersTab from "./OrdersTab";
+import DishTab from "./DishTab";
+import MenuTab from "./MenuTab";
+import OfferTab from "./OfferTab";
+import AddView from "../views/AddView";
+import OrderView from "../views/OrderView";
+import DishFormView from "../forms/DishFormView";
+import MenuFormView from "../forms/MenuFormView";
+import OfferFormView from "../forms/OfferFormView";
+import SettingsCredentialsForm from "../forms/SettingsCredentialsForm";
+import SettingsPersonalInformationForm from "../forms/SettingsPersonalInformationForm";
+import SettingsAddressForm from "../forms/SettingsAddressForm";
+import SettingsOrderInformationForm from "../forms/SettingsOrderInformationForm";
+import React from "react";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
+
+export default function MainTabNavigator(){
+    return(
+        <Tab.Navigator
+            initialRouteName="Signin"
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name === 'Home') {
+                        iconName = 'home-outline'
+                    }else if (route.name === 'Orders') {
+                        iconName = 'basket-outline';
+                    }else if (route.name === 'Dishes') {
+                        iconName = 'restaurant-outline';
+                    }else if (route.name === 'Menus') {
+                        iconName = 'book-outline';
+                    }else if (route.name === 'Offers') {
+                        iconName = 'pricetag-outline';
+                    }else if (route.name === 'Add') {
+                        iconName = 'add-circle-outline';
+                    }else if (route.name === 'Balance') {
+                        iconName = 'cash-outline';
+                    }
+                    // You can return any component that you like here!
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarButton: [
+                    "OrderView",
+                    "DishFormView",
+                    "MenuFormView",
+                    "SettingsCredentialsForm",
+                    "SettingsPersonalInformationForm",
+                    "SettingsAddressForm",
+                    "SettingsOrderInformationForm",
+                    "Balance",
+                    "OfferFormView",
+                ].includes(route.name)
+                    ? () => {
+                        return null;
+                    }
+                    : undefined,
+            })}
+            tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+            }}
+        >
+            <Tab.Screen name="Home" component={HomeStack} />
+            <Tab.Screen name="Orders" component={OrdersTab} options={{ tabBarBadge: 3 }} />
+            <Tab.Screen name="Dishes" component={DishTab} />
+            <Tab.Screen name="Menus" component={MenuTab} />
+            <Tab.Screen name="Offers" component={OfferTab} />
+            <Tab.Screen name="Add" component={AddView} />
+            <Tab.Screen name="OrderView" component={OrderView}/>
+            <Tab.Screen name="DishFormView" component={DishFormView}/>
+            <Tab.Screen name="MenuFormView" component={MenuFormView}/>
+            <Tab.Screen name="OfferFormView" component={OfferFormView}/>
+            <Tab.Screen name="SettingsCredentialsForm" component={SettingsCredentialsForm}/>
+            <Tab.Screen name="SettingsPersonalInformationForm" component={SettingsPersonalInformationForm}/>
+            <Tab.Screen name="SettingsAddressForm" component={SettingsAddressForm}/>
+            <Tab.Screen name="SettingsOrderInformationForm" component={SettingsOrderInformationForm}/>
+        </Tab.Navigator>
+    )
+}
