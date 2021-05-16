8951b4cf5d3822db5419670ea82609693b93c5eb
tout_yong <twotell75@gmail.com>
2021-05-16 21:35:40 +0200

    Removing unused import.

--- source/App.js
+++ source/App.js
@@ -1,28 +1,8 @@
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppearanceProvider } from 'react-native-appearance'
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import OrdersTab from "./tab/OrdersTab";
-import DishTab from "./tab/DishTab";
-import BalanceTab from "./tab/BalanceTab";
-import HomeStack from "./stack/HomeStack";
-import OrderView from "./views/OrderView";
-import DishFormView from "./forms/DishFormView";
-import MenuFormView from "./forms/MenuFormView";
-import SettingsCredentialsForm from "./forms/SettingsCredentialsForm";
-import SettingsPersonalInformationForm from "./forms/SettingsPersonalInformationForm";
-import SettingsAddressForm from "./forms/SettingsAddressForm";
-import SettingsOrderInformationForm from "./forms/SettingsOrderInformationForm";
-import MenuTab from "./tab/MenuTab";
-import OfferTab from "./tab/OfferTab";
-import OfferFormView from "./forms/OfferFormView";
-import AddView from "./views/AddView";
 import LoginFormView from "./forms/LoginFormView";
 import {createStackNavigator} from "@react-navigation/stack";
-import HomeView from "./views/HomeView";
-import all_constants from "./constants";
-import SettingsView from "./views/SettingsView";
 import MainTabNavigator from "./tab/MainTabNavigator";
 
 const Stack = createStackNavigator();
