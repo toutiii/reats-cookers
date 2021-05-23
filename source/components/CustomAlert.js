49bad1eb57814f229bb59a4c7cb19a97f07d3a5e
tout_yong <twotell75@gmail.com>
2021-05-23 20:13:59 +0200

    Removing debug code.

--- source/components/CustomAlert.js
+++ source/components/CustomAlert.js
@@ -3,7 +3,6 @@ import React from "react";
 import {View} from "react-native";
 
 export default function CustomAlert ({...props}){
-    console.log(props)
     return(
         <View style={{flex: 1}}>
             <AwesomeAlert
