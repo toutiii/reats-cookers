c1753626dfcb3ee102123504a4d0d7c1632836c1
tout_yong <twotell75@gmail.com>
2021-05-23 20:52:12 +0200

    To easily print a clean screen title and give to user a way to go back to the previous screen.

--- source/App.js
+++ source/App.js
@@ -5,6 +5,7 @@ import LoginFormView from "./forms/LoginFormView";
 import {createStackNavigator} from "@react-navigation/stack";
 import MainTabNavigator from "./tab/MainTabNavigator";
 import ForgottenPasswordFormView from "./forms/ForgottenPasswordFormView";
+import all_constants from "./constants";
 
 const Stack = createStackNavigator();
 
@@ -20,7 +21,7 @@ export default class App extends Component {
                         initialRouteName='Signin'
                     >
                         <Stack.Screen name="Signin" component={LoginFormView} options={{headerShown: false}} />
-                        <Stack.Screen name="ForgottenPassword" component={ForgottenPasswordFormView} />
+                        <Stack.Screen name="ForgottenPassword" component={ForgottenPasswordFormView} options={{title: all_constants.label.form.forgotten_password.title}} />
                         <Stack.Screen name="MainTabNavigator" component={MainTabNavigator} />
                     </Stack.Navigator>
                 </NavigationContainer>
