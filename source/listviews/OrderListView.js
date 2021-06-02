7ae6b906f9a81f0abdb2e705968605f819922edb
tout_yong <twotell75@gmail.com>
2021-06-02 20:29:30 +0200

    Archived orders hold an 'archived' tag which is absent in other orders.

--- source/listviews/OrderListView.js
+++ source/listviews/OrderListView.js
@@ -4,6 +4,7 @@ import OrderButton from "../button/OrderButton"
 import styles_order from "../styles/styles-order"
 import {getOrders} from "../helpers/order_helpers";
 import {getData} from "../helpers/global_helpers";
+import all_constants from "../constants";
 
 
 export default class OrderListView extends Component {
@@ -11,12 +12,19 @@ export default class OrderListView extends Component {
         super(props);
     }
 
+    getOrderList = () => {
+        if (this.props.route.params.tag === all_constants.tag.orders.archived){
+            return this.props.route.params.item;
+        }
+        return getOrders();
+    }
+
     render() {
         return(
             <View style={styles_order.container}>
                 <OrderButton
                     order_list_data={getData(
-                        getOrders(),
+                        this.getOrderList(),
                         this.props.route.params.tag,
                         undefined,
                         'order_tag',
