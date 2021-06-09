cd2a8f8db30e86ee255594d1adcfa6345a76020b
tout_yong <twotell75@gmail.com>
2021-06-09 19:51:28 +0200

    To stay centered in case of 2 lines text.

--- source/components/Balance.js
+++ source/components/Balance.js
@@ -8,13 +8,13 @@ export default function Balance({...props}) {
     return(
         <View style={styles_balance.sub_container}>
             <View style={styles_balance.order_date}>
-                <Text style={{fontSize: 18}}>{props.order_date}</Text>
+                <Text style={{fontSize: 18, textAlign: 'center'}}>{props.order_date}</Text>
             </View>
             <View style={styles_balance.order_number}>
                 <Text style={{fontSize: 18}}>{all_constants.order.infos.number}{props.order_number}</Text>
             </View>
             <View style={styles_balance.order_balance}>
-                <Text style={{fontSize: 18}}>{props.order_balance}{all_constants.currency_symbol}</Text>
+                <Text style={{fontSize: 18, textAlign: 'center'}}>{props.order_balance}{all_constants.currency_symbol}</Text>
             </View>
         </View>
     )
