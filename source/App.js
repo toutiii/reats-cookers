192f322f50f38a31d56c448447901ebccd7876a8
tout_yong <twotell75@gmail.com>
2023-01-26 14:40:51 +0100

    react-native-appearance has been deleted since Expo SDK 43.

--- source/App.js
+++ source/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { AppearanceProvider } from 'react-native-appearance'
 import LoginFormView from "./forms/LoginFormView";
-import {createStackNavigator} from "@react-navigation/stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import MainTabNavigator from "./tab/MainTabNavigator";
 import ForgottenPasswordFormView from "./forms/ForgottenPasswordFormView";
 import all_constants from "./constants";
@@ -10,31 +9,29 @@ import all_constants from "./constants";
 const Stack = createStackNavigator();
 
 export default class App extends Component {
-    render () {
+    render() {
         return (
-            <AppearanceProvider>
-                <NavigationContainer>
-                    <Stack.Navigator
-                        initialRouteName='Signin'
-                    >
-                        <Stack.Screen
-                            name="Signin"
-                            component={LoginFormView}
-                            options={{headerShown: false}}
-                        />
-                        <Stack.Screen
-                            name="ForgottenPassword"
-                            component={ForgottenPasswordFormView}
-                            options={{title: all_constants.label.form.forgotten_password.title}}
-                        />
-                        <Stack.Screen
-                            name="MainTabNavigator"
-                            component={MainTabNavigator}
-                            options={{headerShown: false}}
-                        />
-                    </Stack.Navigator>
-                </NavigationContainer>
-            </AppearanceProvider>
+            <NavigationContainer>
+                <Stack.Navigator
+                    initialRouteName='MainTabNavigator'
+                >
+                    <Stack.Screen
+                        name="Signin"
+                        component={LoginFormView}
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="ForgottenPassword"
+                        component={ForgottenPasswordFormView}
+                        options={{ title: all_constants.label.form.forgotten_password.title }}
+                    />
+                    <Stack.Screen
+                        name="MainTabNavigator"
+                        component={MainTabNavigator}
+                        options={{ headerShown: false }}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
         )
     }
 }
