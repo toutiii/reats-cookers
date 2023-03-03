da430d96f93362ed2c624fcf6e3449519110c1ca
lettevoueto <lettevoueto.jimmy@gmail.com>
2023-03-03 16:59:33 +0100

    For better design of Orders

--- source/components/NewOrder.js
+++ source/components/NewOrder.js
@@ -1,35 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 import { Divider } from "react-native-paper";
 export default function NewOrder() {
   return (
-    <View
-      style={{
-        flex: 1,
-        borderColor: "purple",
-        borderWidth: 4,
-        flexDirection: "row",
-        alignItems: "center",
-      }}
-    >
+    <View style={{ flex: 1 }}>
       <View
         style={{
-          borderColor: "red",
-          borderWidth: 2,
           flex: 1,
-          margin: "1%",
-          aspectRatio: 1,
+          flexDirection: "row",
+          marginTop: "8%",
         }}
-      />
-      <View
-        style={{
-          borderColor: "green",
-          borderWidth: 2,
-          borderRadius: 20,
-          flex: 2,
-          margin: "1%",
-          aspectRatio: 2,
-        }}
-      />
+      >
+        <View
+          style={{
+            borderColor: "red",
+            borderWidth: 2,
+            flex: 1,
+            margin: "1%",
+            aspectRatio: 1,
+          }}
+        />
+        <View
+          style={{
+            borderColor: "green",
+            borderWidth: 2,
+            borderRadius: 20,
+            flex: 2,
+            margin: "1%",
+            aspectRatio: 2,
+          }}
+        />
+      </View>
+      <Divider />
     </View>
   );
 }
