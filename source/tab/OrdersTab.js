99f3af8bc2ed8d6c5978b81fd8441fa76796a600
tout_yong <twotell75@gmail.com>
2023-03-03 14:31:17 +0100

    The History feature has been delayed.

--- source/tab/OrdersTab.js
+++ source/tab/OrdersTab.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import all_constants from "../constants";
-import HistoryFormView from "../forms/HistoryFormView";
-import HistoryStack from "../stack/HistoryStack";
 import OrderFlatList from "../flatlist/OrderFlatList";
 
 const Tab = createMaterialTopTabNavigator();
@@ -33,12 +31,6 @@ export default function OrdersTab() {
         component={OrderFlatList}
         options={{ title: all_constants.tab.order_tab.title.cancelled }}
         initialParams={{ tag: all_constants.tag.orders.canceled }}
-      />
-      <Tab.Screen
-        name="HistoryStack"
-        component={HistoryStack}
-        options={{ title: all_constants.tab.order_tab.title.history }}
-        initialParams={{ tag: all_constants.tag.orders.archived }}
       />
     </Tab.Navigator>
   );
