4e272844bc4e0da5609ed0d66153c106b04bed41
tout_yong <twotell75@gmail.com>
2023-03-20 01:13:37 +0100

    To not overlap status bar.

--- source/App.js
+++ source/App.js
@@ -5,33 +5,35 @@ import { createStackNavigator } from "@react-navigation/stack";
 import ForgottenPasswordFormView from "./forms/ForgottenPasswordFormView";
 import all_constants from "./constants";
 import { MainDrawerNavigator } from "./drawer/MainDrawerNavigator";
-
+import { SafeAreaView } from "react-native-safe-area-context";
 const Stack = createStackNavigator();
 
 export default class App extends Component {
   render() {
     return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="MainDrawerNavigator">
-          <Stack.Screen
-            name="Signin"
-            component={LoginFormView}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="ForgottenPassword"
-            component={ForgottenPasswordFormView}
-            options={{
-              title: all_constants.label.form.forgotten_password.title,
-            }}
-          />
-          <Stack.Screen
-            name="MainDrawerNavigator"
-            component={MainDrawerNavigator}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <SafeAreaView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="MainDrawerNavigator">
+            <Stack.Screen
+              name="Signin"
+              component={LoginFormView}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="ForgottenPassword"
+              component={ForgottenPasswordFormView}
+              options={{
+                title: all_constants.label.form.forgotten_password.title,
+              }}
+            />
+            <Stack.Screen
+              name="MainDrawerNavigator"
+              component={MainDrawerNavigator}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
     );
   }
 }
