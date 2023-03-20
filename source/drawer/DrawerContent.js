7740d19a8e746f102445b4b2d9fe69ba6dd3dcaf
tout_yong <twotell75@gmail.com>
2023-03-20 01:01:05 +0100

    Used by MainDrawerNavigator.

--- source/drawer/DrawerContent.js
+++ source/drawer/DrawerContent.js
@@ -1,44 +1,41 @@
 import React from "react";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { DrawerContentScrollView } from "@react-navigation/drawer";
 import SettingsView from "../views/SettingsView";
 import all_constants from "../constants";
 import HomeView from "../views/HomeView";
 import CurrentWeekOrderView from "../views/CurrentWeekOrderView";
 import SplitBalanceView from "../views/SplitBalanceView";
 import SplitStatsView from "../views/SplitStatsView";
+import { Drawer } from "react-native-paper";
 
-const Drawer = createDrawerNavigator();
-
-export default function MainDrawerNavigator() {
+export default function MainDrawerContent(props) {
   return (
-    <Drawer.Navigator
-      screenOptions={{ drawerPosition: "left", headerShown: false }}
-    >
-      <Drawer.Screen
+    <DrawerContentScrollView {...props}>
+      <Drawer.Item
         name="Home"
         component={HomeView}
         options={{ title: all_constants.main_drawer_navigator.titles.home }}
       />
-      <Drawer.Screen
+      <Drawer.Item
         name="OrdersTab"
         component={CurrentWeekOrderView}
         options={{ title: all_constants.main_drawer_navigator.titles.orders }}
       />
-      <Drawer.Screen
+      <Drawer.Item
         name="BalanceTab"
         component={SplitBalanceView}
         options={{ title: all_constants.main_drawer_navigator.titles.balance }}
       />
-      <Drawer.Screen
+      <Drawer.Item
         name="Settings"
         component={SettingsView}
         options={{ title: all_constants.main_drawer_navigator.titles.settings }}
       />
-      <Drawer.Screen
+      <Drawer.Item
         name="StatsView"
         component={SplitStatsView}
         options={{ title: all_constants.main_drawer_navigator.titles.stats }}
       />
-    </Drawer.Navigator>
+    </DrawerContentScrollView>
   );
 }
