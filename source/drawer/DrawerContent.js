411b0c1f747e0ca6c844f13e13cdab997171a268
tout_yong <twotell75@gmail.com>
2023-03-21 14:43:04 +0100

    Improving main drawer content.

--- source/drawer/DrawerContent.js
+++ source/drawer/DrawerContent.js
@@ -1,41 +1,172 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import {
+  DrawerContentComponentProps,
+  DrawerContentOptions,
+  DrawerContentScrollView,
+  DrawerItem,
+} from "@react-navigation/drawer";
 import React from "react";
-import { DrawerContentScrollView } from "@react-navigation/drawer";
-import SettingsView from "../views/SettingsView";
-import all_constants from "../constants";
-import HomeView from "../views/HomeView";
-import CurrentWeekOrderView from "../views/CurrentWeekOrderView";
-import SplitBalanceView from "../views/SplitBalanceView";
-import SplitStatsView from "../views/SplitStatsView";
-import { Drawer } from "react-native-paper";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import {
+  Avatar,
+  Caption,
+  Drawer,
+  Paragraph,
+  Switch,
+  Text,
+  Title,
+  TouchableRipple,
+  useTheme,
+} from "react-native-paper";
+import Animated from "react-native-reanimated";
+import ToggleView from "../views/ToggleView";
 
-export default function MainDrawerContent(props) {
+export default function DrawerContent(props) {
+  const paperTheme = useTheme();
+
   return (
     <DrawerContentScrollView {...props}>
-      <Drawer.Item
-        name="Home"
-        component={HomeView}
-        options={{ title: all_constants.main_drawer_navigator.titles.home }}
-      />
-      <Drawer.Item
-        name="OrdersTab"
-        component={CurrentWeekOrderView}
-        options={{ title: all_constants.main_drawer_navigator.titles.orders }}
-      />
-      <Drawer.Item
-        name="BalanceTab"
-        component={SplitBalanceView}
-        options={{ title: all_constants.main_drawer_navigator.titles.balance }}
-      />
-      <Drawer.Item
-        name="Settings"
-        component={SettingsView}
-        options={{ title: all_constants.main_drawer_navigator.titles.settings }}
-      />
-      <Drawer.Item
-        name="StatsView"
-        component={SplitStatsView}
-        options={{ title: all_constants.main_drawer_navigator.titles.stats }}
-      />
+      <Animated.View
+        //@ts-ignore
+        style={[
+          styles.drawerContent,
+          {
+            backgroundColor: paperTheme.colors.surface,
+          },
+        ]}
+      >
+        <View style={styles.userInfoSection}>
+          <TouchableOpacity
+            style={{ marginLeft: 10 }}
+            onPress={() => {
+              props.navigation.toggleDrawer();
+            }}
+          >
+            <Avatar.Image
+              source={require("../images/mum_test.jpg")}
+              size={60}
+            />
+          </TouchableOpacity>
+        </View>
+        <Drawer.Section style={styles.drawerSection}>
+          <ToggleView />
+        </Drawer.Section>
+
+        <Drawer.Section title="Cuisine">
+          <DrawerItem
+            icon={({ color, size }) => (
+              <MaterialCommunityIcons
+                name="bookmark-outline"
+                color={color}
+                size={size}
+              />
+            )}
+            label="Mes plats"
+            onPress={() => {}}
+          />
+          <DrawerItem
+            icon={({ color, size }) => (
+              <MaterialCommunityIcons
+                name="bookmark-outline"
+                color={color}
+                size={size}
+              />
+            )}
+            label="Mes menus"
+            onPress={() => {}}
+          />
+        </Drawer.Section>
+
+        <Drawer.Section title="Marketing">
+          <DrawerItem
+            icon={({ color, size }) => (
+              <MaterialCommunityIcons
+                name="bookmark-outline"
+                color={color}
+                size={size}
+              />
+            )}
+            label="Mes promotions"
+            onPress={() => {}}
+          />
+        </Drawer.Section>
+
+        <Drawer.Section title="Préférences">
+          <DrawerItem
+            icon={({ color, size }) => (
+              <MaterialCommunityIcons
+                name="bookmark-outline"
+                color={color}
+                size={size}
+              />
+            )}
+            label="Connexion"
+            onPress={() => {}}
+          />
+          <DrawerItem
+            icon={({ color, size }) => (
+              <MaterialCommunityIcons
+                name="bookmark-outline"
+                color={color}
+                size={size}
+              />
+            )}
+            label="Commandes"
+            onPress={() => {}}
+          />
+          <DrawerItem
+            icon={({ color, size }) => (
+              <MaterialCommunityIcons
+                name="bookmark-outline"
+                color={color}
+                size={size}
+              />
+            )}
+            label="Compte"
+            onPress={() => {}}
+          />
+        </Drawer.Section>
+      </Animated.View>
     </DrawerContentScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  drawerContent: {
+    flex: 1,
+  },
+  userInfoSection: {
+    paddingLeft: 20,
+  },
+  title: {
+    marginTop: 20,
+    fontWeight: "bold",
+  },
+  caption: {
+    fontSize: 14,
+    lineHeight: 14,
+  },
+  row: {
+    marginTop: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  section: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginRight: 15,
+  },
+  paragraph: {
+    fontWeight: "bold",
+    marginRight: 3,
+  },
+  drawerSection: {
+    marginTop: 15,
+  },
+  preference: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+  },
+});
