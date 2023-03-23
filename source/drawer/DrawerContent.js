64a739f54ef53682c8ef668ca8d4347764f58e4e
tout_yong <twotell75@gmail.com>
2023-03-23 14:40:06 +0100

    Removing unused styles.

--- source/drawer/DrawerContent.js
+++ source/drawer/DrawerContent.js
@@ -166,35 +166,4 @@ const styles = StyleSheet.create({
   userInfoSection: {
     paddingLeft: 20,
   },
-  title: {
-    marginTop: 20,
-    fontWeight: "bold",
-  },
-  caption: {
-    fontSize: 14,
-    lineHeight: 14,
-  },
-  row: {
-    marginTop: 20,
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  section: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginRight: 15,
-  },
-  paragraph: {
-    fontWeight: "bold",
-    marginRight: 3,
-  },
-  drawerSection: {
-    marginTop: 15,
-  },
-  preference: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingVertical: 12,
-    paddingHorizontal: 16,
-  },
 });
