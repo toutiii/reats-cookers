13b7662c16744ddab63edbb429e95026aa2c5f7e
lettevoueto <lettevoueto.jimmy@gmail.com>
2023-04-04 14:02:55 +0200

    Format on save.

--- source/views/OrderView.js
+++ source/views/OrderView.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { ActivityIndicator, Animated, View,Text } from "react-native";
+import { ActivityIndicator, Animated, View, Text } from "react-native";
 import styles_order_view from "../styles/styles-order-view";
 import HorizontalLine from "../components/HorizontalLine";
 import all_constants from "../constants";
@@ -9,7 +9,13 @@ import DishModal from "../modals/DishModal";
 import CustomAlert from "../components/CustomAlert";
 import styles_form from "../styles/styles-form";
 import { callBackEnd } from "../api/fetch";
-import { AntDesign, Ionicons,FontAwesome,MaterialIcons,MaterialCommunityIcons } from '@expo/vector-icons'; 
+import {
+  AntDesign,
+  Ionicons,
+  FontAwesome,
+  MaterialIcons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 export default class OrderView extends Component {
   constructor(props) {
     super(props);
@@ -177,11 +183,21 @@ export default class OrderView extends Component {
                   margin: "2%",
                   aspectRatio: 6,
                   flexDirection: "row",
-                  alignItems:"center"
+                  alignItems: "center",
                 }}
               >
-                <AntDesign name="user" size={30} color="black" style={{paddingRight:"1%"}} />
-                <Text>{all_constants.order.infos.owner} {this.props.route.params.item.order_owner} le {this.props.route.params.item.order_date} à {this.props.route.params.item.order_hour} </Text>
+                <AntDesign
+                  name="user"
+                  size={30}
+                  color="black"
+                  style={{ paddingRight: "1%" }}
+                />
+                <Text>
+                  {all_constants.order.infos.owner}{" "}
+                  {this.props.route.params.item.order_owner} le{" "}
+                  {this.props.route.params.item.order_date} à{" "}
+                  {this.props.route.params.item.order_hour}{" "}
+                </Text>
               </View>
               <View
                 style={{
@@ -189,25 +205,20 @@ export default class OrderView extends Component {
                   margin: "2%",
                   aspectRatio: 6,
                   flexDirection: "row",
-                  alignItems:"center"
-                }}
-              ><FontAwesome name="money" size={30} color="black" style={{paddingRight:"1%"}}/>
-              <Text>{all_constants.order.infos.amount} {this.props.route.params.item.order_amount} {all_constants.currency_symbol}</Text>
-              </View>
-              <View
-                style={{
-                  flex: 1,
-                  margin: "2%",
-                  aspectRatio: 6,
-                  flexDirection: "row",
-                  alignItems:"center"
+                  alignItems: "center",
                 }}
               >
-              {this.props.route.params.item.order_status === all_constants.order.status.approved? (
-                <><AntDesign name="checkcircle" size={30} color="black" style={{paddingRight:"1%"}}/><Text> {all_constants.order.infos.approved_label} {this.props.route.params.item.order_accept_date} à {this.props.route.params.item.order_accept_hour} </Text></>
-              ):this.props.route.params.item.order_status === all_constants.order.status.canceled ?(
-                <><MaterialIcons name="cancel" size={30} color="black" style={{paddingRight:"1%"}}/><Text>{all_constants.order.infos.canceled_label} {this.props.route.params.item.order_cancel_date} à {this.props.route.params.item.order_cancel_hour}</Text></>
-              ):(<><FontAwesome name="hourglass-half" size={24} color="black" style={{paddingRight:"1%"}} /><Text>{this.props.route.params.item.order_status}</Text></>)}
+                <FontAwesome
+                  name="money"
+                  size={30}
+                  color="black"
+                  style={{ paddingRight: "1%" }}
+                />
+                <Text>
+                  {all_constants.order.infos.amount}{" "}
+                  {this.props.route.params.item.order_amount}{" "}
+                  {all_constants.currency_symbol}
+                </Text>
               </View>
               <View
                 style={{
@@ -215,26 +226,100 @@ export default class OrderView extends Component {
                   margin: "2%",
                   aspectRatio: 6,
                   flexDirection: "row",
-                  alignItems:"center"
+                  alignItems: "center",
                 }}
-              ><MaterialIcons name="delivery-dining" size={30} color="black" style={{paddingRight:"1%"}}/>
-              {this.props.route.params.item.order_status === all_constants.order.status.approved?(
-                <Text>{all_constants.order.infos.delivered_label} {this.props.route.params.item.order_delivery_date} à {this.props.route.params.item.order_picking_hour} </Text>
-              ):this.props.route.params.item.order_status === all_constants.order.status.canceled ?(
-                <Text>Livraison {this.props.route.params.item.order_status}</Text>
-              ):(
-                <Text>Livraison en attente de confirmation</Text>
-              )}</View>
+              >
+                {this.props.route.params.item.order_status ===
+                all_constants.order.status.approved ? (
+                  <>
+                    <AntDesign
+                      name="checkcircle"
+                      size={30}
+                      color="black"
+                      style={{ paddingRight: "1%" }}
+                    />
+                    <Text>
+                      {" "}
+                      {all_constants.order.infos.approved_label}{" "}
+                      {this.props.route.params.item.order_accept_date} à{" "}
+                      {this.props.route.params.item.order_accept_hour}{" "}
+                    </Text>
+                  </>
+                ) : this.props.route.params.item.order_status ===
+                  all_constants.order.status.canceled ? (
+                  <>
+                    <MaterialIcons
+                      name="cancel"
+                      size={30}
+                      color="black"
+                      style={{ paddingRight: "1%" }}
+                    />
+                    <Text>
+                      {all_constants.order.infos.canceled_label}{" "}
+                      {this.props.route.params.item.order_cancel_date} à{" "}
+                      {this.props.route.params.item.order_cancel_hour}
+                    </Text>
+                  </>
+                ) : (
+                  <>
+                    <FontAwesome
+                      name="hourglass-half"
+                      size={24}
+                      color="black"
+                      style={{ paddingRight: "1%" }}
+                    />
+                    <Text>{this.props.route.params.item.order_status}</Text>
+                  </>
+                )}
+              </View>
               <View
                 style={{
                   flex: 1,
                   margin: "2%",
                   aspectRatio: 6,
                   flexDirection: "row",
-                  alignItems:"center"
+                  alignItems: "center",
                 }}
-              ><MaterialCommunityIcons name="google-maps" size={30} color="black" style={{paddingRight:"1%"}}/>
-              <Text>4 rue de la grande ferme lieusaint 77127</Text></View>
+              >
+                <MaterialIcons
+                  name="delivery-dining"
+                  size={30}
+                  color="black"
+                  style={{ paddingRight: "1%" }}
+                />
+                {this.props.route.params.item.order_status ===
+                all_constants.order.status.approved ? (
+                  <Text>
+                    {all_constants.order.infos.delivered_label}{" "}
+                    {this.props.route.params.item.order_delivery_date} à{" "}
+                    {this.props.route.params.item.order_picking_hour}{" "}
+                  </Text>
+                ) : this.props.route.params.item.order_status ===
+                  all_constants.order.status.canceled ? (
+                  <Text>
+                    Livraison {this.props.route.params.item.order_status}
+                  </Text>
+                ) : (
+                  <Text>Livraison en attente de confirmation</Text>
+                )}
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  margin: "2%",
+                  aspectRatio: 6,
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <MaterialCommunityIcons
+                  name="google-maps"
+                  size={30}
+                  color="black"
+                  style={{ paddingRight: "1%" }}
+                />
+                <Text>4 rue de la grande ferme lieusaint 77127</Text>
+              </View>
             </View>
             <HorizontalLine line_width={3} />
           </View>
