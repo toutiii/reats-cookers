4d86c8592ec42be18dbf3643b78f7aaeaeb0b6d7
lettevoueto <lettevoueto.jimmy@gmail.com>
2023-04-05 11:33:23 +0200

    For delete the gray headband.

--- source/styles/styles-order-view.js
+++ source/styles/styles-order-view.js
@@ -5,14 +5,13 @@ let styles_order_view;
 export default styles_order_view = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: "20%",
-    marginBottom: "5%",
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "white",
   },
   button_container: {
     flex: 1,
-    margin: "5%",
+    alignSelf: "center",
     alignItems: "center",
     width: "95%",
   },
