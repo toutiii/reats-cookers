f3cf19ce3b54f7336d9781663df49f3f1184e624
tout_yong <twotell75@gmail.com>
2023-04-17 00:11:24 +0200

    MenuFormView has been deleted.

--- source/tab/MainTabNavigator.js
+++ source/tab/MainTabNavigator.js
@@ -47,7 +47,6 @@ export default function MainTabNavigator() {
         tabBarButton: [
           "OrderView",
           "DishFormView",
-          "MenuFormView",
           "SettingsCredentialsForm",
           "SettingsPersonalInformationForm",
           "SettingsAddressForm",
