424114985f5fcf11dfba6ef6384a190c50b3841c
lettevoueto <lettevoueto.jimmy@gmail.com>
2023-04-17 16:47:59 +0200

    To change the button closure .

--- source/modals/DishModal.js
+++ source/modals/DishModal.js
@@ -18,36 +18,29 @@ export default function DishModal({ ...props }) {
         backdropTransitionOutTiming={600}
         isVisible={props.state}
       >
-        <View style={{ flex: 1, alignItems: "center", marginTop: "10%" }}>
-          <CustomButton
-            label={all_constants.modal.dish_modal.hide}
-            backgroundColor="tomato"
-            label_color="white"
-            height={50}
-            border_width={3}
-            border_radius={30}
-            font_size={17}
-            onPress={props.onPressCloseModal}
-          />
-        </View>
+        <View style={{ flex: 1, backgroundColor: "white", padding: 10 }}>
+          <View style={{ flex: 1 }}>
+            <Button title="Close" onPress={props.onPressCloseModal} />
+          </View>
 
-        <View style={{ flex: 8 }}>
-          <FlatList
-            data={props.modal_data}
-            renderItem={({ item }) => (
-              <DishForModal
-                key={item.id}
-                dish_photo={item.photo}
-                dish_name={item.dish_name}
-                dish_rating={item.dish_rating}
-                dish_unit_price={item.dish_unit_price}
-                dish_style={item.dish_style}
-                dish_quantity={item.dish_quantity}
-                dish_order_date={item.dish_order_date}
-                dish_order_status={item.dish_order_status}
-              />
-            )}
-          />
+          <View style={{ flex: 8 }}>
+            <FlatList
+              data={props.modal_data}
+              renderItem={({ item }) => (
+                <DishForModal
+                  key={item.id}
+                  dish_photo={item.photo}
+                  dish_name={item.dish_name}
+                  dish_rating={item.dish_rating}
+                  dish_unit_price={item.dish_unit_price}
+                  dish_style={item.dish_style}
+                  dish_quantity={item.dish_quantity}
+                  dish_order_date={item.dish_order_date}
+                  dish_order_status={item.dish_order_status}
+                />
+              )}
+            />
+          </View>
         </View>
       </Modal>
     </View>
