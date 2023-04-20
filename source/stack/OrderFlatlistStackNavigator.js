fa2071b2dcb65f512f22cde5f4cf0b948e6ee6d8
tout_yong <twotell75@gmail.com>
2023-04-21 00:01:56 +0200

    Better UX to link each item of a flatlist through a stack navigation.

---
+++ source/stack/OrderFlatlistStackNavigator.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import OrderFlatList from "../flatlist/OrderFlatList";
+import OrderView from "../views/OrderView";
+
+const Stack = createStackNavigator();
+
+export default function OrderFlatlistStackNavigator() {
+  return (
+    <Stack.Navigator initialRouteName="FlatlistStackNavigatorHome">
+      <Stack.Screen
+        name="FlatlistStackNavigatorHome"
+        component={OrderFlatList}
+        options={{
+          headerShown: false,
+          headerMode: "none",
+        }}
+      />
+      <Stack.Screen
+        name="FlatlistStackNavigatorOrderView"
+        component={OrderView}
+        options={{
+          headerTitle: "",
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
