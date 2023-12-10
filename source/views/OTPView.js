62f6ecfd9c8790de273a9bd7af00df4c0c2b7710
Toutel <twotell75@gmail.com>
2023-12-10 19:47:06 +0000

    Resolve "improve-otp-view"

--- source/views/OTPView.js
+++ source/views/OTPView.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { TextInput, View } from "react-native";
+import { Platform, StyleSheet, TextInput, View } from "react-native";
 
 import CustomButton from "../button/CustomButton";
 import all_constants from "../constants";
@@ -8,15 +8,14 @@ import { callBackEnd } from "../api/callBackend";
 import CustomAlert from "../components/CustomAlert";
 
 export default function OTPView({ ...props }) {
-  const [OTPValue, setOTPValue] = useState("");
+  const [OTPValue, setOTPValue] = useState(["", "", "", "", "", ""]);
   const [showAlert, setShowAlert] = React.useState(false);
   const [isRequestSuccessful, setIsRequestSuccessful] = React.useState(false);
 
   async function verifyOTP() {
     let data = new FormData();
-    data.append("otp", OTPValue);
+    data.append("otp", OTPValue.join(""));
     data.append("phone", props.route.params.item.phone);
-
     const result = await callBackEnd(
       data,
       "http://192.168.1.85:8000/api/v1/cookers/otp-verify/",
@@ -27,6 +26,20 @@ export default function OTPView({ ...props }) {
     setIsRequestSuccessful(result.ok);
     setShowAlert(true);
   }
+
+  const inputs = [];
+
+  const handleOtpChange = (value, index) => {
+    const newOtp = [...OTPValue];
+    newOtp[index] = value;
+    setOTPValue(newOtp); // Move focus to the next box if the current one has a value
+    if (value && index < newOtp.length - 1) {
+      inputs[index + 1].focus();
+    }
+    if (value === "" && index >= 1) {
+      inputs[index - 1].focus();
+    }
+  };
 
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
@@ -50,20 +63,30 @@ export default function OTPView({ ...props }) {
           }}
         />
       </View>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-        }}
-      >
-        <TextInput
-          backgroundColor="darkgrey"
-          height={40}
-          onChangeText={(value) => {
-            setOTPValue(value);
-          }}
-          keyboardType="numeric"
-        />
+      <View style={styles.container}>
+        {OTPValue.map((digit, index) => (
+          <TextInput
+            key={index}
+            style={styles.box}
+            autoComplete={
+              Platform.OS === "android" && index === 0 ? "sms-otp" : "off"
+            }
+            textContentType={
+              Platform.OS === "ios" && index === 0 ? "oneTimeCode" : "none"
+            }
+            height={40}
+            onChangeText={(value) => handleOtpChange(value, index)}
+            keyboardType="numeric"
+            cursorColor="green"
+            textAlign="center"
+            maxLength={1}
+            value={digit}
+            autoFocus={index === 0 ? true : false}
+            ref={(input) => {
+              inputs[index] = input;
+            }}
+          />
+        ))}
       </View>
 
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -83,3 +106,21 @@ export default function OTPView({ ...props }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  box: {
+    borderWidth: 2,
+    borderColor: "black",
+    width: 35,
+    height: 50,
+    margin: "3%",
+    textAlign: "center",
+    fontSize: 20,
+  },
+});
