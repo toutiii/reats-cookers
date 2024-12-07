c6071b5b0d4235780aacbb28398446484c58dd07
Toutel <twotell75@gmail.com>
2024-12-07 17:44:19 +0000

    Resolve "update-dependencies"

---
+++ source/.prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	arrowParens: 'always',
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	jsxSingleQuote: true,
+	singleQuote: true,
+	semi: true,
+	printWidth: 100,
+	useTabs: true,
+	tabWidth: 4,
+	arrowParens: 'always',
+	endOfLine: 'lf',
+	trailingComma: 'all',
+};
--- source/tab/MainTabNavigator.js
+++ source/tab/MainTabNavigator.js
@@ -16,79 +16,67 @@ import OrdersHistoryStackNavigator from "../stack/OrdersHistoryStackNavigator";
 const Tab = createBottomTabNavigator();
 
 export default function MainTabNavigator() {
-  return (
-    <Tab.Navigator
-      initialRouteName={all_constants.tab.main_tab_navigator.Home}
-      screenOptions={({ route }) => ({
-        activeTintColor: "tomato",
-        inactiveTintColor: "gray",
-        headerShown: false,
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-          if (route.name === all_constants.tab.main_tab_navigator.Home) {
-            iconName = "home-outline";
-          } else if (
-            route.name === all_constants.tab.main_tab_navigator.OrderFlatList
-          ) {
-            iconName = "basket-outline";
-          } else if (
-            route.name === all_constants.tab.main_tab_navigator.DishFlatList
-          ) {
-            iconName = "restaurant-outline";
-          } else if (
-            route.name === all_constants.tab.main_tab_navigator.DrinkFlatList
-          ) {
-            iconName = "wine-sharp";
-          }
-          // You can return any component that you like here!
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarButton: [
-          "OrderView",
-          "DishForm",
-          "DrinkForm",
-          "SettingsPersonalInformationForm",
-          "SettingsAddressForm",
-          "OrdersHistory",
-        ].includes(route.name)
-          ? () => {
-              return null;
-            }
-          : undefined,
-      })}
-    >
-      <Tab.Screen
-        name={all_constants.tab.main_tab_navigator.Home}
-        component={Dashboard}
-      />
-      <Tab.Screen
-        name={all_constants.tab.main_tab_navigator.OrderFlatList}
-        component={OrderFlatlistStackNavigator}
-        options={{ tabBarBadge: 3 }}
-      />
-      <Tab.Screen
-        name={all_constants.tab.main_tab_navigator.DishFlatList}
-        component={DishFlatlistStackNavigator}
-      />
-      <Tab.Screen
-        name={all_constants.tab.main_tab_navigator.DrinkFlatList}
-        component={DrinkFlatlistStackNavigator}
-      />
+    return (
+        <Tab.Navigator
+            initialRouteName={all_constants.tab.main_tab_navigator.Home}
+            screenOptions={({ route }) => ({
+                activeTintColor: "tomato",
+                inactiveTintColor: "gray",
+                headerShown: false,
+                tabBarIcon: ({ color, size }) => {
+                    let iconName;
+                    if (route.name === all_constants.tab.main_tab_navigator.Home) {
+                        iconName = "home-outline";
+                    } else if (route.name === all_constants.tab.main_tab_navigator.OrderFlatList) {
+                        iconName = "basket-outline";
+                    } else if (route.name === all_constants.tab.main_tab_navigator.DishFlatList) {
+                        iconName = "restaurant-outline";
+                    } else if (route.name === all_constants.tab.main_tab_navigator.DrinkFlatList) {
+                        iconName = "wine-sharp";
+                    }
+                    // You can return any component that you like here!
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarButton: [
+                    "OrderView",
+                    "DishForm",
+                    "DrinkForm",
+                    "SettingsPersonalInformationForm",
+                    "SettingsAddressForm",
+                    "OrdersHistory",
+                ].includes(route.name)
+                    ? () => {
+                        return null;
+                    }
+                    : undefined,
+            })}
+        >
+            <Tab.Screen name={all_constants.tab.main_tab_navigator.Home} component={Dashboard} />
+            <Tab.Screen
+                name={all_constants.tab.main_tab_navigator.OrderFlatList}
+                component={OrderFlatlistStackNavigator}
+                // options={{ tabBarBadge: 3 }}
+            />
+            <Tab.Screen
+                name={all_constants.tab.main_tab_navigator.DishFlatList}
+                component={DishFlatlistStackNavigator}
+            />
+            <Tab.Screen
+                name={all_constants.tab.main_tab_navigator.DrinkFlatList}
+                component={DrinkFlatlistStackNavigator}
+            />
 
-      <Tab.Screen name="OrderView" component={OrderView} />
-      <Tab.Screen name="DishForm" component={DishForm} />
-      <Tab.Screen name="DrinkForm" component={DrinkForm} />
+            <Tab.Screen name='OrderView' component={OrderView} />
+            <Tab.Screen name='DishForm' component={DishForm} />
+            <Tab.Screen name='DrinkForm' component={DrinkForm} />
 
-      <Tab.Screen
-        name="SettingsPersonalInformationForm"
-        component={SettingsPersonalInformationForm}
-      />
-      <Tab.Screen name="SettingsAddressForm" component={SettingsAddressForm} />
+            <Tab.Screen
+                name='SettingsPersonalInformationForm'
+                component={SettingsPersonalInformationForm}
+            />
+            <Tab.Screen name='SettingsAddressForm' component={SettingsAddressForm} />
 
-      <Tab.Screen
-        name="OrdersHistory"
-        component={OrdersHistoryStackNavigator}
-      />
-    </Tab.Navigator>
-  );
+            <Tab.Screen name='OrdersHistory' component={OrdersHistoryStackNavigator} />
+        </Tab.Navigator>
+    );
 }
