11aab2b12196cc94bb0347d4e1dd42063a9c96f8
tout'it <70380468+toutiii@users.noreply.github.com>
2025-04-06 15:34:12 +0200

    [BUILD] Setting up conf for build pipeline (#3)
    Co-authored-by: tout_yong <twotell75@gmail.com>

---
+++ source/app.config.js
@@ -0,0 +1,58 @@
+export default {
+    expo: {
+        name: "reats-cookers",
+        slug: "reats-cookers",
+        version: "1.0.0",
+        orientation: "portrait",
+        icon: "./assets/icon.png",
+        splash: {
+            image: "./assets/splash.png",
+            resizeMode: "contain",
+            backgroundColor: "#ffffff",
+        },
+        updates: {
+            fallbackToCacheTimeout: 0,
+            url: "https://u.expo.dev/6df1673a-3c4d-4188-b913-b619a786ee34",
+        },
+        assetBundlePatterns: [
+            "**/*"
+        ],
+        ios: {
+            supportsTablet: true,
+            bundleIdentifier: "com.reats.cookers",
+        },
+        android: {
+            adaptiveIcon: {
+                foregroundImage: "./assets/adaptive-icon.png",
+                backgroundColor: "#FFFFFF",
+            },
+            package: "com.reats.cookers",
+        },
+        web: {
+            favicon: "./assets/favicon.png",
+        },
+        plugins: [
+            "expo-secure-store"
+        ],
+        newArchEnabled: true,
+        extra: {
+            eas: {
+                projectId: "5e8c63f9-94eb-4a4f-8125-239918afb898",
+            },
+            ENV: process.env.ENV,
+            apiBaseUrl:
+                process.env.EXPO_PUBLIC_PRODUCTION_API_BASE_URL ??
+                process.env.EXPO_PUBLIC_STAGING_API_BASE_URL ??
+                process.env.EXPO_PUBLIC_DEVELOPMENT_API_BASE_URL,
+            apiKeyBackend:
+                process.env.PRODUCTION_API_KEY ??
+                process.env.STAGING_API_KEY ??
+                process.env.DEVELOPMENT_API_KEY,
+            appOriginHeader: process.env.EXPO_PUBLIC_APP_ORIGIN,
+        },
+        owner: "tout_it",
+        runtimeVersion: {
+            policy: "appVersion",
+        },
+    },
+};
