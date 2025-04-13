3d6f3b0c0dc3cf36e5ec85fa35341750edd5faca
tout_yong <twotell75@gmail.com>
2025-04-13 11:40:10 +0200

    Reworking deployment

--- source/app.config.js
+++ source/app.config.js
@@ -2,7 +2,7 @@ export default {
     expo: {
         name: "reats-cookers",
         slug: "reats-cookers",
-        version: "1.0.0",
+        version: "0.1.0",
         orientation: "portrait",
         icon: "./assets/icon.png",
         splash: {
